585fa17c57ec2ba68d62975cbdcfb4e1a0228322
Dev Diego <151096440+Dev-Diego@users.noreply.github.com>
2024-08-29 17:31:10 -0500

    © Powered By Sunlight Team ⚡︎

--- plugins/rpg-robxp.js
+++ plugins/rpg-robxp.js
@@ -2,26 +2,26 @@ const ro = 3000;
 const handler = async (m, {conn, usedPrefix, command}) => {
   const time = global.db.data.users[m.sender].lastrob + 7200000;
   if (new Date - global.db.data.users[m.sender].lastrob < 7200000) {
-  conn.reply(m.chat, `*🚩 𝑯𝒆𝒚! 𝑬𝒔𝒑𝒆𝒓𝒂 ${msToTime(time - new Date())} 𝒑𝒂𝒓𝒂 𝒗𝒐𝒍𝒗𝒆𝒓 𝒂 𝒓𝒐𝒃𝒂𝒓*`, m, rcanal);
+  conn.reply(m.chat, `*🚩 Hey! Espera ${msToTime(time - new Date())} para volver a robar*`, m, rcanal);
   return;
   }
   let who;
   if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false;
   else who = m.chat;
   if (!who) {
-  conn.reply(m.chat, `*🚩 𝑬𝒕𝒊𝒒𝒖𝒆𝒕𝒂 𝒂 𝒂𝒍𝒈𝒖𝒊𝒆𝒏 𝒑𝒂𝒓𝒂 𝒓𝒐𝒃𝒂𝒓.*`, m, rcanal)
+  conn.reply(m.chat, `*🚩 Etiqueta al usuario.*`, m, fake)
   return;
     };
   if (!(who in global.db.data.users)) { 
-  conn.reply(m.chat, `*🚩 𝑬𝒍 𝒖𝒔𝒖𝒂𝒓𝒊𝒐 𝒏𝒐 𝒔𝒆 𝒆𝒏𝒄𝒖𝒆𝒏𝒕𝒓𝒂 𝒆𝒏 𝒎𝒊 𝒃𝒂𝒔𝒆 𝒅𝒆 𝒅𝒂𝒕𝒐𝒔 😕.*`, m, rcanal)
+  conn.reply(m.chat, `*🚩 El usuario no está en mi base de datos.*`, m, rcanal)
 return;
   }
   const users = global.db.data.users[who];
   const rob = Math.floor(Math.random() * ro);
-  if (users.exp < rob) return conn.reply(m.chat, `😔 @${who.split`@`[0]} 𝒕𝒊𝒆𝒏𝒆 𝒎𝒆𝒏𝒐𝒔 𝒅𝒆 *${ro} xp*\n𝑵𝒐 𝒓𝒐𝒃𝒆𝒔 𝒂 𝒖𝒏 𝒑𝒐𝒃𝒓𝒆 v":`, m, {mentions: [who]});
+  if (users.exp < rob) return conn.reply(m.chat, `😔 @${who.split`@`[0]} Tiene menos de *${ro} xp*\nNo le robes a un pobre :v":`, m, {mentions: [who]});
   global.db.data.users[m.sender].exp += rob;
   global.db.data.users[who].exp -= rob;
-  conn.reply(m.chat, `*🚩 𝑹𝒐𝒃𝒂𝒔𝒕𝒆 ${rob} XP 𝒂 @${who.split`@`[0]}*`, m, {mentions: [who]});
+  conn.reply(m.chat, `*🚩 Robastes ${rob} XP A @${who.split`@`[0]}*`, m, {mentions: [who]});
   global.db.data.users[m.sender].lastrob = new Date * 1;
 };
 handler.help = ['rob'];
