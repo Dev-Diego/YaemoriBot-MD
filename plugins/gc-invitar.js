c6a6c5083b39b73a794e8ae8f3fd34fb50fcdb04
ChissF <theyaemoribot@gmail.com>
2024-08-31 21:05:55 -0500

    © Powered By Sunlight Team ⚡︎

--- plugins/gc-invitar.js
+++ plugins/gc-invitar.js
@@ -1,11 +1,11 @@
 let handler = async (m, { conn, args, text, usedPrefix, command }) => {
-if (!text) return m.reply(`🍟 Ingrese el número al que quiere enviar una invitación al grupo\n\n🚩 Ejemplo :\n*${usedPrefix + command}* 573218138672`)
-if (text.includes('+')) return m.reply('🚩 Ingrese el número todo junto sin el *+*')
+if (!text) await m.reply(`🍟 Ingrese el número al que quiere enviar una invitación al grupo\n\n🚩 Ejemplo :\n*${usedPrefix + command}* 573218138672`)
+if (text.includes('+')) await m.reply('🚩 Ingrese el número todo junto sin el *+*')
 if (isNaN(text)) return m.reply('🍟 Ingrese sólo números más su código de país sin espacios')
 let group = m.chat
 let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
 await conn.reply(text+'@s.whatsapp.net', `🍟 *INVITACIÓN A GRUPO*\n\nUn usuario te invitó a unirte a este grupo \n\n${link}`, m, {mentions: [m.sender]})
-m.reply(`🍟 Se envió un enlace de invitación al usuario.`) 
+await m.reply(`🍟 Se envió un enlace de invitación al usuario.`) 
 
 }
 handler.help = ['invite *<numero>*']
