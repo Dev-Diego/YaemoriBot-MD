ce331f221a0211f676b6366485abb5e8d9fbcd76
ChissF <theyaemoribot@gmail.com>
2024-09-07 16:29:28 -0500

    © Powered By Sunlight Team ⚡︎

--- plugins/rpg-shop.js
+++ plugins/rpg-shop.js
@@ -1,4 +1,4 @@
-const xpperestrellas = 350;
+const xppercookies = 350;
 const handler = async (m, {conn, command, args}) => {
   let count = command.replace(/^buy/i, '');
   count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].exp / xppercookies) : parseInt(count) : args[0] ? parseInt(args[0]) : 1;
