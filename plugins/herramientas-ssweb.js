d2f4f38c1e10b0d1fc35277d403888adba291a17
ChissF <theyaemoribot@gmail.com>
2024-09-06 21:36:42 -0500

    © Powered By Sunlight Team ⚡︎

--- plugins/herramientas-ssweb.js
+++ plugins/herramientas-ssweb.js
@@ -1,8 +1,10 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, command, args }) => {
 if (!args[0]) return conn.reply(m.chat, '⚠️ *Ingrese el Link de una página.*', m, fake)
+await m.react(rwait)
 let ss = await (await fetch(`https://violetics.pw/api/media/ssweb?apikey=beta&url=${args[0]}`)).buffer()
-conn.sendFile(m.chat, ss, 'error.png', args[0], fkontak)}
+conn.sendFile(m.chat, ss, 'error.png', args[0], fkontak)
+await m.react(done)}
 handler.help = ['ssweb', 'ss']
 handler.tags = ['tools']
 handler.command = ['ssweb', 'ss']
