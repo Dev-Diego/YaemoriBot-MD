d0318394e0afe24fe0c7bddfb0ec49064b5488de
Dev Diego <151096440+Dev-Diego@users.noreply.github.com>
2024-09-06 22:05:12 -0500

    © Powered By Sunlight Team ⚡︎

--- plugins/sticker-wm.js
+++ plugins/sticker-wm.js
@@ -2,23 +2,24 @@ import { addExif } from '../lib/sticker.js'
 
 
 let handler = async (m, { conn, text }) => {
-  if (!m.quoted) throw '*⚠ El sticker!*'
+  if (!m.quoted) await m.reply('*⚠ El sticker!*')
   let stiker = false
   try {
     let [packname, ...author] = text.split('|')
     author = (author || []).join('|')
     let mime = m.quoted.mimetype || ''
-    if (!/webp/.test(mime)) throw '⚠️ *Responde a un sticker.*'
+    if (!/webp/.test(mime)) await m.reply('⚠️ *Responde a un sticker.*')
     let img = await m.quoted.download()
-    if (!img) throw '⚠ *Responde a un sticker!*'
+    if (!img) await m.reply('⚠ *Responde a un sticker!*')
     stiker = await addExif(img, packname || '', author || '')
   } catch (e) {
     console.error(e)
     if (Buffer.isBuffer(e)) stiker = e
   } finally {
-  await conn.reply(m.chat, global.wait, m, fake)
+  await m.react(rwait)
     if (stiker) conn.sendFile(m.chat, stiker, 'wm.webp', '', m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: packname, body: dev, mediaType: 2, sourceUrl: redes, thumbnail: icons}}}, { quoted: fkontal })
-    else throw '⚠️ *_La conversión falló._*'
+   await m.react(done)
+    else throw '⚠️ *La conversión falló.*'
   }
 }
 handler.help = ['take *<nombre>|<autor>*']
