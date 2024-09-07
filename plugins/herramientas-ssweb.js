195fa9387e0820cbcc8855b9e7886bfd7cffa8da
ChissF <theyaemoribot@gmail.com>
2024-09-06 21:38:16 -0500

    © Powered By Sunlight Team ⚡︎

--- plugins/herramientas-ssweb.js
+++ plugins/herramientas-ssweb.js
@@ -1,10 +1,12 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, command, args }) => {
-if (!args[0]) return conn.reply(m.chat, '⚠️ *Ingrese el Link de una página.*', m, fake)
+if (!args[0]) return conn.reply(m.chat, '⚠️ *Ingrese el Link de una página.*', m, rcanal)
 await m.react(rwait)
 let ss = await (await fetch(`https://violetics.pw/api/media/ssweb?apikey=beta&url=${args[0]}`)).buffer()
 conn.sendFile(m.chat, ss, 'error.png', args[0], fkontak)
-await m.react(done)}
+await m.react(done)
+} catch {
+return conn.reply(m.chat, '🚩 Ocurrió un error.', m, fake)}
 handler.help = ['ssweb', 'ss']
 handler.tags = ['tools']
 handler.command = ['ssweb', 'ss']
