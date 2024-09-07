1b17b2dc4db1d1d63e29233ad9c9cd63652be31b
Dev Diego <151096440+Dev-Diego@users.noreply.github.com>
2024-09-06 21:45:17 -0500

    © Powered By Sunlight Team ⚡︎

--- plugins/herramientas-ssweb.js
+++ plugins/herramientas-ssweb.js
@@ -9,7 +9,7 @@ title: packname,
 body: wm,
 previewType: 0, thumbnail: icons, 
 sourceUrl: channel }}})
-let ss = await (await fetch(`https://violetics.pw/api/media/ssweb?apikey=beta&url=${args[0]}`)).buffer()
+let ss = await (await fetch(`https://image.thum.io/get/fullpage/${args[0]}`)).buffer()
 conn.sendFile(m.chat, ss, 'error.png', args[0], fkontak)
 await m.react(done)
 } catch {
