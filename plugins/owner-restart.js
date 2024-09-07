024b3dfe8bb69c7d91c87d1e6e327757d9697449
Dev Diego <151096440+Dev-Diego@users.noreply.github.com>
2024-09-07 14:39:40 -0500

    © Powered By Sunlight Team ⚡︎

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -2,7 +2,7 @@ import { spawn } from 'child_process'
 
 var handler = async (m, { conn, isROwner, text }) => {
 
-if (!process.send) throw 'Dont: node start.js\nDo: node index.js'
+if (!process.send) throw 'Dont: node sunlight.js\nDo: node index.js'
 if (conn.user.jid == conn.user.jid) {
 await conn.reply(m.chat, '🍟 *R E I N I C I A N D O* 🍟', m, rcanal, )
 process.send('reset')
