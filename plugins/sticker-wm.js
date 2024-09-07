a572530cdb1d6a0daf140d295e2b2d40423dad71
Dev Diego <151096440+Dev-Diego@users.noreply.github.com>
2024-09-06 22:21:16 -0500

    © Powered By Sunlight Team ⚡︎

--- plugins/sticker-wm.js
+++ plugins/sticker-wm.js
@@ -18,8 +18,8 @@ let handler = async (m, { conn, text }) => {
   } finally {
   // await conn.reply(m.chat, global.wait, m)
      if (stiker) conn.sendFile(m.chat, stiker, 'wm.webp', '', m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: packname, body: dev, mediaType: 2, sourceUrl: redes, thumbnail: icons}}}, { quoted: fkontak })
-  await m.react(done)
-     return m.reply('⚠️ *La conversión falló.*')
+  // await m.react(done)
+     else throw '⚠️ *La conversión falló.*'
   }
 }
 handler.help = ['take *<nombre>|<autor>*']
