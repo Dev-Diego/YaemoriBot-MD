dc1fd45ad9c4cc9df6b5c4bdbb772dbd303ca555
Dev Diego <151096440+Dev-Diego@users.noreply.github.com>
2024-09-07 21:33:48 -0500

    © Powered By Sunlight Team ⚡︎

--- handler.js
+++ handler.js
@@ -418,6 +418,11 @@ global.db.data.users[m.sender].spam = new Date * 1
                 this.msgqueque.splice(quequeIndex, 1)
         }
         let user, stats = global.db.data.stats
+       if (utente.muto == true) {
+       let bang = m.key.id
+       let cancellazzione = m.key.participant
+      await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: cancellazzione }})
+        }
         if (m) {
             if (m.sender && (user = global.db.data.users[m.sender])) {
                 user.exp += m.exp
