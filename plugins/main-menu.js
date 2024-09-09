57b746f1a691c51edc1533461095c5eb0f803697
ChissF <theyaemoribot@gmail.com>
2024-09-08 19:59:57 -0500

    © Powered By Sunlight Team ⚡︎

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -49,7 +49,7 @@ const defaultMenu = {
 \t*L I S T A  -  D E  -  C O M A N D O S* 
 `.trimStart(),
     header: '*•/• %category •/•*\n',
-  body: '☄︎ %cmd',
+  body: '✰ %cmd',
   footer: '',
   after: `> ${dev}`,
 }
