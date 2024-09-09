57010e0ad4ac16716e3a02966173c1274e49f9b3
ChissF <theyaemoribot@gmail.com>
2024-09-08 19:57:45 -0500

    ⍴᥆ᥕᥱrᥱძ ᑲᥡ ᥡᥲᥱm᥆rіᑲ᥆𝗍 🚩

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -49,7 +49,7 @@ const defaultMenu = {
 \t*L I S T A  -  D E  -  C O M A N D O S* 
 `.trimStart(),
     header: '*•/• %category •/•*\n',
-  body: '🌺 %cmd',
+  body: '☄︎ %cmd',
   footer: '',
   after: `> ${dev}`,
 }
