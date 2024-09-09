f9d33348e15beac022fbf9148bd3887544f6fc5c
ChissF <theyaemoribot@gmail.com>
2024-09-09 09:56:59 -0500

    © Powered By Sunlight Team ⚡︎

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -1,4 +1,4 @@
-/*import { promises } from 'fs'
+import { promises } from 'fs'
 import { join } from 'path'
 import fetch from 'node-fetch'
 import { xpRange } from '../lib/levelling.js'
@@ -208,70 +208,4 @@ switch(hour){
   case 22: hour = 'Bᴜᴇɴᴀs Nᴏᴄʜᴇs 🌙'; break;
   case 23: hour = 'Bᴜᴇɴᴀs Nᴏᴄʜᴇs 🌃'; break;
 }
-  var greeting = hour;*/
-
-//By:AzamiJs, GataNina-Li
-
-import fs from 'fs'
-import { parsePhoneNumber } from 'libphonenumber-js'
-import fetch from 'node-fetch'
-import { xpRange } from '../lib/levelling.js'
-const { levelling } = '../lib/levelling.js'
-import PhoneNumber from 'awesome-phonenumber'
-import { promises } from 'fs'
-import { join } from 'path'
-import ct from 'countries-and-timezones'
-import moment from 'moment-timezone'
-import translate from '@vitalets/google-translate-api'
-const { generateWAMessageFromContent, proto } = (await import('@whiskeysockets/baileys')).default
-
-var handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text, command }) => {
-
-try {
-
-let user = conn.getName(m.sender)
-
-let pp = await conn.profilePictureUrl(conn.user.jid).catch(_ => 'https://qu.ax/Jzst.jpg')
-
-let fechaMoment, formatDate, nombreLugar, ciudad = null; const phoneNumber = '+' + m.sender; const parsedPhoneNumber = parsePhoneNumber(phoneNumber); const countryCode = parsedPhoneNumber.country; const countryData = ct.getCountry(countryCode); const timezones = countryData.timezones; const zonaHoraria = timezones.length > 0 ? timezones[0] : 'UTC'; moment.locale('es'); let lugarMoment = moment().tz(zonaHoraria); if (lugarMoment) { fechaMoment = lugarMoment.format('llll [(]a[)]'); formatDate = fechaMoment.charAt(0).toUpperCase() + fechaMoment.slice(1); nombreLugar = countryData.name; const partes = zonaHoraria.split('/'); ciudad = partes[partes.length - 1].replace(/_/g, ' '); } else { lugarMoment = moment().tz('Bogotá/Colombia'); fechaMoment = lugarMoment.format('llll [(]a[)]'); formatDate = fechaMoment.charAt(0).toUpperCase() + fechaMoment.slice(1); nombreLugar = 'Bogotá'; ciudad = 'Ciudad De Colombia' }
-
-//FAKES
-let a = {'key': {'participants': '0@s.whatsapp.net', 'fromMe': false, 'id': '3B64558B07848BD81108C1D14712018E'}, 'message': {'locationMessage': {'name': `${user}`, 'jpegThumbnail': await (await fetch(pp)).buffer(), 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${wm},;;;\nFN:${wm},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'}
-const ftrol = { key : { remoteJid: 'status@broadcast', participant : '0@s.whatsapp.net' }, message: { orderMessage: { itemCount : 2023, status: 1, surface : 1, message: `${user}!`, orderTitle: `▮Menu ▸`, sellerJid: '0@s.whatsapp.net' }}}
-const fload = { key : { message: `YaemoriBot-MD 🌱` + `\nDev-Diego`, thumbnail: await (await fetch(pp)).buffer(), sellerJid: '0@s.whatsapp.net' }}
-
-await m.react('🚩')
-
-let menu = `🌸 !Hola¡ ${user}
-
-!allmenu
-*(Para ver el menú completo)*
-
-!menu2
-*(Para ver el menú de audios)*
-
-!owner
-*(Para ver el propietario del bot)*`
-
-await conn.reply(m.chat, menu, fkontak, { contextInfo: { externalAdReply: {title: packname, body: dev, sourceUrl: redeshost, thumbnail: await (await fetch(pp)).buffer() }}})
-
-} catch {
-await m.react(error)
-conn.reply(m.chat, `🚩 *Ocurrió un fallo*`, m, fake, )
-console.log(e)
-}}
-handler.help = ['menu']
-handler.tags = ['main']
-handler.command = ['menu']
-handler.register = true
-
-export default handler
-
-function clockString(ms) {
-let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
-let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
-
-function pickRandom(list) {
-return list[Math.floor(Math.random() * list.length)]}
+  var greeting = hour;
