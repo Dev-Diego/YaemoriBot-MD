79909c77a41dcbd96e2082033481e85d23a3a4fc
ChissF <theyaemoribot@gmail.com>
2024-09-11 11:07:58 -0500

    © Powered By Sunlight Team ⚡︎

--- plugins/main-grupos.js
+++ plugins/main-grupos.js
@@ -32,7 +32,7 @@ let grupos = `*Hola!, te invito a unirte a los grupos oficiales de del Bot para 
 
 > ${dev}`
 
-await conn.sendMessage(m.chat, {text: grupos, contextInfo: { forwardingScore: 999, isForwarded: true, forwardedNewsletterMessageInfo: { newsletterName: 'YaemoriBot 🌱', newsletterJid: "120363263466636910@newsletter", }, externalAdReply: { title: packname, body: dev, thumbnailUrl: 'https://qu.ax/BiDQ.jpg', sourceUrl: redes, mediaType: 1, renderLargerThumbnail: true }}}, {quoted: fkontak})
+await conn.sendFile(m.chat, imagen3, "yaemori.jpg", grupos, fkontak, null, rcanal)
 
 await m.react(emojis)
 
