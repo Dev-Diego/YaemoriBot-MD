f000a9e23d62089a47642cb1af7f5cce3abac0a4
Dev Diego <151096440+Dev-Diego@users.noreply.github.com>
2024-09-14 18:48:55 -0500

    © Powered By Sunlight Team ⚡︎

--- plugins/_antiarab.js
+++ plugins/_antiarab.js
@@ -6,7 +6,7 @@ handler.before = async function(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner
   if (!m.isGroup) return !1;
   const chat = global.db.data.chats[m.chat];
   const bot = global.db.data.settings[conn.user.jid] || {};
-  if (isBotAdmin && chat.antiArab2 && !isAdmin && !isOwner && !isROwner && bot.restrict) {
+  if (isBotAdmin && chat.antiArab && !isAdmin && !isOwner && !isROwner && bot.restrict) {
     if (m.sender.startsWith('212' || '212')) {
       m.reply(`『✦』Los numeros: *212*, se van eliminados de este grupo.`);
       const responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
