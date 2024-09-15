f08c78089f8a48462eeab431ecf5ac341b6de3d4
Dev Diego <151096440+Dev-Diego@users.noreply.github.com>
2024-09-15 17:33:12 -0500

    © Powered By Sunlight Team ⚡︎

--- plugins/fun-piropo.js
+++ plugins/fun-piropo.js
@@ -4,12 +4,14 @@ import db from '../lib/database.js'
 
 let handler = async (m, { conn, text}) => {
 
-conn.reply(m.chat, '🚩 *¡Buscando un piropo!*', m, {
+//
+//conn.reply(m.chat, '🚩 *¡Buscando un piropo!*', m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
 title: packname,
 body: wm,
 previewType: 0, thumbnail: icons,
 sourceUrl: channel }}})
+//
 
 let hasil = Math.floor(Math.random() * 5000)
 
