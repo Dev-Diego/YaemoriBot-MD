d721d5437334c0aad5de6839c155a85ea5e3dc06
ChissF <theyaemoribot@gmail.com>
2024-09-15 18:32:43 -0500

    © Powered By Sunlight Team ⚡︎

--- plugins/owner-leavegc.js
+++ plugins/owner-leavegc.js
@@ -1,20 +1,8 @@
 let handler = async (m, { conn, text, command }) => {
 let id = text ? text : m.chat  
-let pp = 'https://telegra.ph/file/5ab1ca8bf65c1ddb36c20.mp4'
-await conn.sendMessage(m.chat, { video: { url: pp }, gifPlayback: true, caption: '《✧》 *Ai Yaemori* Abandona El Grupo, Fué Genial Estar Aquí', mentions: [m.sender] }, { quoted: fkontak })
-await conn.groupLeave(id)}
-handler.command = ['leave', 'leavegc', 'salir']
-handler.group = true
-handler.rowner = true
-export default handler
-
-export default handler
-
-/*let handler = async (m, { conn, text, command }) => {
-let id = text ? text : m.chat  
 let chat = global.db.data.chats[m.chat]
 chat.welcome = false
-await conn.reply(id, `🚩 *Ai Yaemori* Abandona El Grupo, Fué Genial Estar Aquí 👋`) 
+await conn.reply(id, `《✧》 *Ai Yaemori* Abandona El Grupo, Fué Genial Estar Aquí`) 
 await conn.groupLeave(id)
 try {  
 chat.welcome = true
@@ -25,4 +13,4 @@ return console.log(e)
 handler.command = ['leave', 'leavegc', 'salir']
 handler.group = true
 handler.rowner = true
-export default handler*/
+export default handler
