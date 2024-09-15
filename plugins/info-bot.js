43917ff851801535e0331d69f5ed46f23fa36d12
ChissF <theyaemoribot@gmail.com>
2024-09-14 19:27:53 -0500

    © Powered By Sunlight Team ⚡︎

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -7,9 +7,9 @@ if (chat.isBaneed) return
 if (/^bot$/i.test(m.text)) {
 conn.reply(m.chat, `🚩 ¡Hola! Soy Ai Yaemori, en que puedo ayudarte hoy?\n\n✰ Usa *!menu* para ver mis comandos.`, m, rcanal, )
 }
-if (/^corin|corín|corinplus|corínplus|corinplushost|corínplushost|plus$/i.test(m.text)) {
+/*if (/^corin|corín|corinplus|corínplus|corinplushost|corínplushost|plus$/i.test(m.text)) {
 conn.reply(m.chat, `🚀 CorinPlus Hosting ¡El plus que necesitas!\n🚩 *Dash:* https://dash.corinplus.com\n🌱 *Panel:* https://ctrl.corinplus.com`, m, rcanal, )
-}
+}*/
 if (/^sexo$/i.test(m.text)) {
 conn.reply(m.chat, `*pervertido* 🫣`, m, rcanal, )
 }
