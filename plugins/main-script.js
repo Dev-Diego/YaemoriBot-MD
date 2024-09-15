86e6b9813e58fef3736d63621923dba4591a5669
Dev Diego <151096440+Dev-Diego@users.noreply.github.com>
2024-09-15 16:46:38 -0500

    © Powered By Sunlight Team ⚡︎

--- plugins/main-script.js
+++ plugins/main-script.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 
 let handler = async (m, { conn, args }) => {
 try {
-let res = await fetch('https://dash-yaemori.vercel.app')
+let res = await fetch('https://api.github.com/repos/Dev-Diego/YaemoriBot-MD')
 
 if (!res.ok) throw new Error('Error al obtener datos del repositorio')
 let json = await res.json()
