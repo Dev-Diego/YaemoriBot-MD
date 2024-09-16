0bfac444f62147bda02a30491ec5e0464090300d
Dev Diego <151096440+Dev-Diego@users.noreply.github.com>
2024-09-15 22:24:27 -0500

    © Powered By Sunlight Team ⚡︎

--- plugins/rg-perfil.js
+++ plugins/rg-perfil.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 var handler = async (m, { conn }) => {
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let pp = await conn.profilePictureUrl(who, 'image').catch(_ => imagen1)
-let { premium, level, cookies, exp, lastclaim, registered, regTime, age, role } = global.db.data.users[m.sender]
+let { premium, level, estrellas, exp, lastclaim, registered, regTime, age, role } = global.db.data.users[m.sender]
 let username = conn.getName(who)
 let noprem = `
 ┏━━━━━━━━━⪩
@@ -22,27 +22,18 @@ let noprem = `
 ┃💖 *Premium:* ${premium ? '✔': '✘'}
 ┗━━━━━━━━━⪩
 `.trim()
-let prem = `
-┏━━━━━━━━━⪩
-┃> * 𝐔𝐒𝐔𝐀𝐑𝐈𝐎 𝐏𝐑𝐄𝐌𝐈𝐔𝐌
-┗━━━━━━━━━⪩
+let prem = `╭──⪩ 𝐔𝐒𝐔𝐀𝐑𝐈𝐎 𝐏𝐑𝐄𝐌𝐈𝐔𝐌 ⪨
+│⧼👤⧽ *ᴜsᴜᴀʀɪᴏ:* 「${username}」
+│⧼💌⧽ *ʀᴇɢɪsᴛʀᴀᴅᴏ:* ${registered ? '✅': '❌'}
+│⧼🔱⧽ *ʀᴏʟ:* Vip 👑
+╰───⪨
 
-┏━━━━━⪩ 𝐈𝐍𝐅𝐎𝐑𝐌𝐀𝐂𝐈𝐎𝐍
-┃⧼☁⧽ *Usuario:* 「${username}」
-┃⧼❄⧽ *Tag:* @${who.replace(/@.+/, '')}
-┃⧼🌟⧽ *Registrado:* ${registered ? '✅': '❌'}
-┃⧼🔱⧽ *Rol:* Vip 👑
-┗━━━━━━━━━━⪩
-
-┏━━━━━⪩ 𝐑𝐄𝐂𝐔𝐑𝐒𝐎𝐒
-┃⧼🍪⧽ *Cookies:* ${cookies}
-┃⧼💥⧽ *Nivek:* ${level}
-┃⧼💫⧽ *Exp:* ${exp}
-┃⧼✨⧽ *Rango:* ${role}
-┗━━━━━━━━━━⪩ 
-
-*𝙽𝙾𝚃𝙰:*
-> 𝙴𝚂𝚃𝙴 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙴𝚂 𝙳𝙴𝚂𝚃𝙰𝙲𝙰𝙳𝙾`.trim()
+╭────⪩ 𝐑𝐄𝐂𝐔𝐑𝐒𝐎𝐒 ⪨
+│⧼🌟⧽ *ᴇsᴛʀᴇʟʟᴀs:* ${estrellas}
+│⧼🔰⧽ *ɴɪᴠᴇʟ:* ${level}
+│⧼💫⧽ *ᴇxᴘᴇʀɪᴇɴᴄɪᴀ:* ${exp}
+│⧼⚜️⧽ *ʀᴀɴɢᴏ:* ${role}
+╰───⪨ *𝓤𝓼𝓾𝓪𝓻𝓲𝓸 𝓓𝓮𝓼𝓽𝓪𝓬𝓪𝓭𝓸* ⪩`.trim()
 conn.sendFile(m.chat, pp, 'perfil.jpg', `${premium ? prem.trim() : noprem.trim()}`, m, rcanal, { mentions: [who] })
 }
 handler.help = ['profile']
