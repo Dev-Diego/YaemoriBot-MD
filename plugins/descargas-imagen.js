bb2774caf9c65d9f0fa70318ddc2d2b1c4fa5e52
Dev Diego <151096440+Dev-Diego@users.noreply.github.com>
2024-09-16 13:33:24 -0500

    © Powered By Sunlight Team ⚡︎

--- plugins/descargas-imagen.js
+++ plugins/descargas-imagen.js
@@ -1,16 +1,18 @@
 import {googleImage} from '@bochilteam/scraper';
 const handler = async (m, {conn, text, usedPrefix, command}) => {
-  if (!text) throw `*🚩 Uso Correcto: ${usedPrefix + command} Ai Yaemori*`;
-  conn.reply(m.chat, '🚩 *Descargando su imagen...*', m, {
-  contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
-  title: packname,
-  body: wm,
-  previewType: 0, thumbnail: icons,
-  sourceUrl: channel }}})
-  const res = await googleImage(text);
-  const image = await res.getRandom();
-  const link = image;
-  conn.sendFile(m.chat, link, 'error.jpg', `*🔎 Resultado De: ${text}*\n> ${textbot}`, m, null, rcanal);
+if (!text) return conn.reply(m.chat, `*🚩 Uso Correcto: ${usedPrefix + command} Ai Yaemori*`, m, rcanal);
+conn.reply(m.chat, '🚩 *Descargando su imagen...*', m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
+title: packname,
+body: wm,
+previewType: 0, thumbnail: icons,
+sourceUrl: channel }}})
+const res = await googleImage(text);
+const image = await res.getRandom();
+const link = image;
+const messages = [['Imagen 1', dev, await res.getRandom(),
+[[]], [[]], [[]], [[]]], ['Imagen 2', dev, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 2', dev, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 4', dev, await res.getRandom(), [[]], [[]], [[]], [[]]]]
+await conn.sendCarousel(m.chat, '🚩 Buscador - Imagenes', `🔍 Resultado de: ${text}`, text, messages, m);
 };
 handler.help = ['imagen <query>'];
 handler.tags = ['buscador', 'tools', 'descargas'];
