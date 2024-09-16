50919c35e2595cb1c77a6de038bda49250d73640
Dev Diego <151096440+Dev-Diego@users.noreply.github.com>
2024-09-15 20:19:41 -0500

    © Powered By Sunlight Team ⚡︎

--- plugins/fun-consejo.js
+++ plugins/fun-consejo.js
@@ -9,11 +9,11 @@ body: wm,
 previewType: 0, thumbnail: icons,
 sourceUrl: channel }}})*/
 
-conn.reply(m.chat, `《✧》» ${pickRandom(global.consejo)}`, 
+conn.reply(m.chat, `『✧』 ${pickRandom(global.consejo)}`, 
 m, 
 { contextInfo:{ externalAdReply: {title: '•🍀 FRASE / CONSEJO 🍀•', 
 body: dev, 
-sourceUrl: redes, 
+sourceUrl: channel, 
 thumbnail: icons }}})
 
 }
