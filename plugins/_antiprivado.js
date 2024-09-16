f451f301c8b05f4bd495ee3fdd866c58571f2acf
ChissF <theyaemoribot@gmail.com>
2024-09-15 20:08:00 -0500

    © Powered By Sunlight Team ⚡︎

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -6,7 +6,7 @@ export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) 
   const chat = global.db.data.chats[m.chat];
   const bot = global.db.data.settings[this.user.jid] || {};
   if (bot.antiPrivate && !isOwner && !isROwner) {
-    await m.reply(`> "🍧 Hola @${m.sender.split`@`[0]}, Lo Siento No Esta 🍭Permitido Escribirme Al Privado 🍫Por Lo Cual Seras Bloqueado/A\n\n> *🍫Puedes Unirte Al Grupo Oficial De La Bot🍿* 👇\n\n\n${grupo}`, false, {mentions: [m.sender]});
+    await m.reply(`《✧》Hola @${m.sender.split`@`[0]}, mi creador a desactivado los comandos en los chats privados el cual serás bloqueado, si quieres usar los comandos del bot te invito a que te unas al grupo principal del bot.\n\n${grupo}`, false, {mentions: [m.sender]});
     await this.updateBlockStatus(m.chat, 'block');
   }
   return !1;
