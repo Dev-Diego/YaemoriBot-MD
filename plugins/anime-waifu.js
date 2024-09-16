bc9185d1091c77658859aa021ba173c0a2e3661e
Dev Diego <151096440+Dev-Diego@users.noreply.github.com>
2024-09-15 20:33:21 -0500

    © Powered By Sunlight Team ⚡︎

--- plugins/anime-waifu.js
+++ plugins/anime-waifu.js
@@ -13,7 +13,7 @@ let res = await fetch('https://api.waifu.pics/sfw/waifu')
 if (!res.ok) return
 let json = await res.json()
 if (!json.url) return 
-await conn.sendFile(m.chat, json.url, 'thumbnail.jpg', '《✧》 *`W A I F U`* 《✧》', fkontak, null, rcanal)
+await conn.sendFile(m.chat, json.url, 'thumbnail.jpg', '《✧》 *W A I F U* 《✧》', fkontak, null, rcanal)
 } catch {
 }}
 handler.help = ['waifu']
