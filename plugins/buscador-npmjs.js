cfd744335b4d68e630032207b899a10fb86db77e
おDiego.xyz <151096440+Dev-Diego@users.noreply.github.com>
2024-09-18 08:30:44 -0500

    Sara ❤️‍🩹

--- plugins/buscador-npmjs.js
+++ plugins/buscador-npmjs.js
@@ -7,27 +7,30 @@ import fetch from 'node-fetch'
 
 let handler = async (m, { text, usedPrefix, command }) => {
 
-if (msg.body.startsWith('search-npmjs')) {
+if (m.body.startsWith('buscador-npmjs')) {
 const packageName = msg.body.split(' ')[1];
 if (!packageName) {
-m.reply('Por favor, proporciona el nombre del paquete que deseas buscar.');
-return;
+m.reply('Por favor, proporciona el nombre del paquete que deseas buscar.')
+return
 }
+
 try {
-const packageInfo = await npmjs.get(packageName);
-const response = `**${packageName}**\n`;
-response += `Version: ${packageInfo.version}\n`;
-response += `Description: ${packageInfo.description}\n`;
-response += `Author: ${packageInfo.author.name}\n`;
-response += `License: ${packageInfo.license}\n`;
-response += `Repository: ${packageInfo.repository.url}\n`;
-m.reply(response);
-} catch (error) {
-m.reply(`Error al buscar el paquete: ${error.message}`);
-}
-}
-});
-client.initialize();
+
+const packageInfo = await npmjs.get(packageName)
+
+const response = `**${packageName}**\n`
+response += `Version: ${packageInfo.version}\n`
+response += `Description: ${packageInfo.description}\n`
+response += `Author: ${packageInfo.author.name}\n`
+response += `License: ${packageInfo.license}\n`
+response += `Repository: ${packageInfo.repository.url}\n`
+m.reply(response)
+await.react(done)
+
+} catch (e) {
+console.log(e)
+await m.react(error)
+m.reply(`Error al buscar el paquete: ${text}`)}}
 
 handler.help = ['npmjs']
 handler.tags = ['buscador']
