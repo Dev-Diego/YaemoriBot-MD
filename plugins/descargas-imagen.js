524e950c4467b5f394ad9d8a0428d1bc2e70c442
おDiego.xyz <theyaemoribot@gmail.com>
2024-09-17 21:47:36 -0500

    ❤️‍🩹

--- plugins/descargas-imagen.js
+++ plugins/descargas-imagen.js
@@ -2,6 +2,7 @@ import {googleImage} from '@bochilteam/scraper';
 
 const handler = async (m, {conn, text, usedPrefix, command}) => {
 if (!text) return conn.reply(m.chat, `*🚩 Uso Correcto: ${usedPrefix + command} Ai Yaemori*`, m, rcanal);
+await m.react(rwait)
 conn.reply(m.chat, '🚩 *Descargando su imagen...*', m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
 title: packname,
@@ -14,6 +15,7 @@ const link = image;
 const messages = [['Imagen 1', dev, await res.getRandom(),
 [[]], [[]], [[]], [[]]], ['Imagen 2', dev, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 2', dev, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 4', dev, await res.getRandom(), [[]], [[]], [[]], [[]]]]
 await conn.sendCarousel(m.chat, `🚩 Resultado de ${text}`, '🔎 Imagen - Descargas', null, messages, m);
+await m.react(done)
 };
 handler.help = ['imagen <query>'];
 handler.tags = ['buscador', 'tools', 'descargas'];
