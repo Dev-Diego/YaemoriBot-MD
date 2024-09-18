0a1f9552ee78007e075406e68bed79aaf42ca69e
おDiego.xyz <theyaemoribot@gmail.com>
2024-09-17 21:09:36 -0500

    おDiego.xyz

--- plugins/buscador-npmjs.js
+++ plugins/buscador-npmjs.js
@@ -4,22 +4,42 @@ By GabrielVz (@glytglobal)
 
 import fetch from 'node-fetch'
 
-let handler = async (m, { text }) => {
-  if (!text) throw '*「 🌟 」INGRESA EL TITULO O NOMBRE DEL SCRAPER DE NMPJS A BUSCAR*\n\n*• EJEMPLO:*\n> */npmjs yt-search*'
-  let res = await fetch(`http://registry.npmjs.com/-/v1/search?text=${text}`)
-  let { objects } = await res.json()
-  if (!objects.length) throw `「 🌟 」 LA BUSQUEDA "${text}" NO FUE ENCONTRADA`
-  let txt = objects.map(({ package: pkg }) => {
-    return 
- `✰ 𝐍𝐨𝐦𝐛𝐫𝐞: ${pkg.name}
-  ✰ 𝐕𝐞𝐫𝐬𝐢𝐨𝐧: V${pkg.version}
-  ✰ 𝐄𝐧𝐥𝐚𝐜𝐞: ${pkg.links.npm}
-  ✰ 𝐃𝐞𝐬𝐜𝐫𝐢𝐩𝐜𝐢𝐨𝐧: ${pkg.description}\n\n\`\`\`----------\`\`\``
-  }).join`\n`
-  m.reply(txt)
-}
+let handler = async (m, { text, usedPrefix, command }) => {
+
+if (!text) return conn.reply(m.chat, `🚩 Escribe el nonbre del scraper.\nEjemplo: ${usedPrefix + command} yt-search`, m, rcanal)
+
+try {
+
+await m.react(rwait)
+conn.reply(m.chat, '🚩 Buscando el scraper....', m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
+title: packname,
+body: dev,
+previewType: 0, thumbnail: icons, 
+sourceUrl: channel }}})
+
+let res = await fetch(`http://registry.npmjs.com/-/v1/search?text=${text}`)
+let { objects } = await res.json()
+
+if (!objects.length) return conn.reply(m.chat, `『✦』 No se encpntró resultado de: ${text}`, m, fake)
+
+let txt = objects.map(({ package: pkg }) => {
+return 
+`✧ Nombre: ${pkg.name}
+ ✧ Versión: V${pkg.version}
+ ✧ Enlace: ${pkg.links.npm}
+ ✧ Descripcion: ${pkg.description}\n\n\`\`\`----------\`\`\``
+}).join`\n`
+
+await conn.reply(m.chat, txt, m, rcanal)
+await m.react(done)
+} catch {
+await conn.reply(m.chat, '🌱 Ocurrió un error', m, fake)
+await m.react(error)
+}}
+
 handler.help = ['npmjs']
-handler.tags = ['search']
-handler.command = /^npmjs?$/i
+handler.tags = ['buscador']
+handler.command = ['npmjs']
 
 export default handler
