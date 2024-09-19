2352121388b41837688a3f6182b873871201f383
おDiego.xyz <theyaemoribot@gmail.com>
2024-09-19 15:51:05 -0500

    Actualizar ff-donarsala.js

--- plugins/ff-donarsala.js
+++ plugins/ff-donarsala.js
@@ -1,4 +1,4 @@
-function handler(m, { groupMetadata }) {
+function handler(m, { groupMetadata, participants }) {
 let psmap = groupMetadata.participants.filter(v => v !== conn.user.jid)
 psmap=psmap.filter(v => v.admin !=='superadmin')
 psmap=psmap.filter(v => v.admin !=='admin')
@@ -9,7 +9,7 @@ let user0 = psmap.getRandom()
 if (psmap == '') return conn.reply(m.chat, `😿 No se ha encontrado usuarios que va donar la sala.`, m, fake)
 
 m.reply(`*${user(user0)},* Le tocó donar sala el dia de hoy. 😅`, null, {
-mentions: [user0, user]
+mentions: [user0]
 })}
 handler.help = ['donarsala']
 handler.tags = ['ff']
