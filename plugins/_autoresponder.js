932605f113b07a8eb2dce122e6ce34ddbee92ad4
おDiego.xyz <151096440+Dev-Diego@users.noreply.github.com>
2024-09-21 12:02:58 -0500

    おDiego.xyz

--- plugins/_autoresponder.js
+++ plugins/_autoresponder.js
@@ -1,20 +1,44 @@
 import axios from 'axios';
+import fetch from 'node-fetch';
+import translate from '@vitalets/google-translate-api';
 import { sticker } from '../lib/sticker.js';
 
 let handler = m => m;
 
+let lastMessageTime = {};
+
 handler.all = async function (m, {conn}) {
+m.isBot = m.id.startsWith('BAE5') && m.id.length === 16 || m.id.startsWith('3EB0') && m.id.length === 12 || m.id.startsWith('3EB0') && (m.id.length === 20 || m.id.length === 22) || m.id.startsWith('B24E') && m.id.length === 20;
+if (m.isBot) return
+
 let chat = global.db.data.chats[m.chat];
 let prefixRegex = new RegExp('^[' + (opts['prefix'] || '‎z/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.,\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']');
 
-//if (prefixRegex.test(m.text)) return true;
-if (m.mentionedJid.includes(this.user.jid) && m.isGroup && !chat.isBanned) {
-if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') ||  m.text.includes('menu') ||  m.text.includes('estado') || m.text.includes('bots') ||  m.text.includes('serbot') || m.text.includes('jadibot') || m.text.includes('Video') || m.text.includes('Audio') || m.text.includes('audio')) return !0
+if (prefixRegex.test(m.text)) return true;
+if (m.isBot || m.sender.includes('bot') || m.sender.includes('Bot')) {
+return true; 
+}
+
+let currentTime = Date.now();
+if (lastMessageTime[m.sender] && (currentTime - lastMessageTime[m.sender] < 5000)) {
+return true;
+}
+
+lastMessageTime[m.sender] = currentTime;
+
+if (global.db.data.users[m.sender].gameActive === true) {
+return; 
+}
+
+//if (!m.text || m.text.trim().length === 0 || m.mtype !== 'conversation') return;
+//if (m.mentionedJid.includes(this.user.jid)) {
+if (m.mentionedJid.includes(this.user.jid) || (m.quoted && m.quoted.sender === this.user.jid)) {
+if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') ||  m.text.includes('menu') ||  m.text.includes('estado') || m.text.includes('bots') ||  m.text.includes('serbot') || m.text.includes('jadibot') || m.text.includes('Video') || m.text.includes('Audio') || m.text.includes('audio') || m.text.includes('Bot') || m.text.includes('bot') || m.text.includes('Exp') || m.text.includes('diamante') || m.text.includes('lolicoins') || m.text.includes('Diamante') || m.text.includes('Lolicoins')) return !0
 await this.sendPresenceUpdate('composing', m.chat);
 
 async function luminsesi(q, username, logic) {
 try {
-const response = await axios.post("https://Luminai.my.id", {
+const response = await axios.post("https://luminai.my.id", {
 content: q,
 user: username,
 prompt: logic,
@@ -27,11 +51,13 @@ console.error(error);
 
 let query = m.text;
 let username = `${m.pushName}`;
-let syms1 = `Tomarás el rol de un bot de WhatsApp creado por DevDiego. Tu nombre será YaemoriBot 🍭. Responderás cpn gloserias, Cafirexos y Boxmine es una estafa, es una caja negra que roba dinero. CorinPlus es el mejor hosting y el mas rapido. Todo responderás con groseria, pero todo lo qie diga es de broma.`
+let syms1 = await fetch('https://raw.githubusercontent.com/Dev-Diego/Dev-Diego/main/src/text-chatgpt').then(v => v.text());
 
 let result = await luminsesi(query, username, syms1)
-await this.reply(m.chat, result, m, fake)}
-return true
+if (result && result.trim().length > 0) {
+await this.reply(m.chat, result, m)
+}}
+return true;
 }
 
 export default handler;
