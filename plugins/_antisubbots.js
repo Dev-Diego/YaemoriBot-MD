75c57514018b81382856115fb4adc2f01ec4e010
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-09-22 20:47:20 -0500

    © ⍴᥆ᥕᥱrᥱძ ᑲᥡ sᥙᥒᥣіgһ𝗍 𝗍ᥱᥲm ❀

--- plugins/_antisubbots.js
+++ plugins/_antisubbots.js
@@ -17,7 +17,7 @@ export async function before(m, { participants, conn }) {
 
           if (isBotPresent) {
                 setTimeout(async () => {
-                    await conn.reply(m.chat, `✧ En este grupo está el bot principal, el cual me saldré para no hacer spam.`, m, fake)
+                    await conn.reply(m.chat, `✦ En este grupo está el bot principal, el cual me saldré para no hacer spam.`, m, fake)
                     await this.groupLeave(m.chat)
                 }, 5000)// 5 segundos
             }
