eae23da0c02d2c44493e1d018a8c1958f3dc8c93
Bananaxd44 <163498656+Bananaxd44@users.noreply.github.com>
2024-09-24 01:22:41 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ import moment from 'moment-timezone'
 
 //BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
 //Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
-global.botNumberCode = '' //Ejemplo: +573218138672
+global.botNumberCode '+59163351397' //Ejemplo: +573218138672
 global.confirmCode = ''
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
