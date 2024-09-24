a9be677ee148b6ebc3fb90d384a686084ea2ea47
Ahmedaligue <158603161+Ahmedaligue@users.noreply.github.com>
2024-09-24 18:49:11 +0100

    Create رياضة.js

---
+++ plugins /رياضة.js
@@ -0,0 +1,38 @@
+import fs from 'fs';
+
+let timeout = 60000;
+let poin = 500;
+
+let handler = async (m, { conn, usedPrefix }) => {
+    conn.tekateki = conn.tekateki ? conn.tekateki : {};
+    let id = m.chat;
+    if (id in conn.tekateki) {
+        conn.reply(m.chat, '❐┃لم يتم الاجابة علي السؤال بعد┃❌ ❯', conn.tekateki[id][0]);
+        throw false;
+    }
+    let tekateki = JSON.parse(fs.readFileSync(`./src/game/رياضة.json`));
+    let json = tekateki[Math.floor(Math.random() * tekateki.length)];
+    let _clue = json.response;
+    let clue = _clue.replace(/[A-Za-z]/g, ''); // Fixed this line
+    let caption = `
+ⷮ ${json.question}
+
+❐↞┇الـوقـت⏳↞ ${(timeout / 1000).toFixed(2)}┇
+❐↞┇الـجـائـزة↞ ${poin} من النقود┇
+*SENKU BOT*
+`.trim();
+    conn.tekateki[id] = [
+       await conn.reply(m.chat, caption, m),
+        json, poin,
+        setTimeout(async () => {
+            if (conn.tekateki[id]) await conn.reply(m.chat, `❮ ⌛┇انتهي الوقت┇⌛❯\n ❐↞┇الاجـابـة✅↞ ${json.response}┇`, conn.tekateki[id][0]);
+            delete conn.tekateki[id];
+        }, timeout)
+    ];
+};
+
+handler.help = ['miku'];
+handler.tags = ['game'];
+handler.command = /^(رياضه)$/i;
+
+export default handler;
