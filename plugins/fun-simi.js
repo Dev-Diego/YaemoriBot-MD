0ae81b26b3a0cb5489b07a3c09dd5c55f8f34983
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-09-25 20:34:08 -0500

    © ⍴᥆ᥕᥱrᥱძ ᑲᥡ sᥙᥒᥣіgһ𝗍 𝗍ᥱᥲm ❀

--- plugins/fun-simi.js
+++ plugins/fun-simi.js
@@ -4,7 +4,7 @@ import fetch from 'node-fetch';
 
 const handler = async (m, {conn, text, command, args, usedPrefix}) => {
 
-if (!text) conn.reply(m.chat, '✧ Te faltó el texto para hablar con la Bot', m, rcanal);
+if (!text) conn.reply(m.chat, '✧ Te faltó el texto para hablar con la Bot', m);
 try {
 // await m.react(emojis);
 const resSimi = await simitalk(text);
