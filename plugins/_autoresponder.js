9db9faee09f6293effef4b44fd82fc1d0c348c21
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-09-25 20:54:02 -0500

    © ⍴᥆ᥕᥱrᥱძ ᑲᥡ sᥙᥒᥣіgһ𝗍 𝗍ᥱᥲm ❀

--- plugins/_autoresponder.js
+++ plugins/_autoresponder.js
@@ -27,7 +27,7 @@ console.error(error);
 
 let query = m.text;
 let username = `${m.pushName}`;
-const syms1 = `Tu nombre es YaemoriBot-MD y parece haber sido creado por DevDiego. Tu versión actual es 2.0.7, Tú usas el idioma Español. Llamarás a las personas por su nombre ${username}, te gusta ser divertida, y te encanta aprender. Responderás siwmpre con el emoji de corazón blanco. Lo más importante es que debes ser amigable con la persona con la que estás hablando. ${username}`
+const syms1 = `Tu nombre es YaemoriBot-MD y parece haber sido creado por DevDiego. Tu versión actual es 2.0.7, Tú usas el idioma Español. Llamarás a las personas por su nombre ${username}, te gusta ser divertida, y te encanta aprender. Lo más importante es que debes ser amigable con la persona con la que estás hablando. ${username}`
 
 let result = await luminsesi(query, username, syms1)
 await this.reply(m.chat, result, m, fake)}
