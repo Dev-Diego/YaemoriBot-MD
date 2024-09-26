f5adc3e29fdd7327f68aa206ffcd8e71aa13895b
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-09-25 21:02:48 -0500

    © ⍴᥆ᥕᥱrᥱძ ᑲᥡ sᥙᥒᥣіgһ𝗍 𝗍ᥱᥲm ❀

--- settings.js
+++ settings.js
@@ -65,9 +65,9 @@ global.avatar = fs.readFileSync('./media/avatar_contact.jpg')
 
 global.grupo = 'https://chat.whatsapp.com/DBV1zXbwYjL43uuI6s2Rhu' //YaemoriBot
 global.grupo2 = 'https://chat.whatsapp.com/EVc2XJuZwjnKKdWUsRYmms' //YaemoriBot2
-global.grupo3 = 'https://chat.whatsapp.com/Fx0yC76BYzRAmiGxZWb9Ho' //team oficial
+global.grupo3 = 'https://chat.whatsapp.com/E8pLRSvjnMHJadRqXXUbaO' //team oficial
 global.grupo4 = 'https://chat.whatsapp.com/Ksd32KH9zFoDmO1PJt44M7' //GataBot & YaemoriBot
-global.grupo5 = 'https://chat.whatsapp.com/EURPjJ3r7aYJPYncJL94Z4' //Curiosity & YaemoriBot
+global.grupo5 = 'https://chat.whatsapp.com/CrcXTQMXHzdAuQPVhaAJlm' //Curiosity & YaemoriBot
 global.channel = 'https://whatsapp.com/channel/0029VaQD7LAJP216tu9liI2A'
 global.channel2 = 'https://whatsapp.com/channel/0029Vam7yUg77qVaz3sIAp0z'
 global.md = 'https://github.com/Dev-Diego/YaemoriBot-MD' 
