249c6f07dac00f22dbe826a04b108fb65a74ed14
SheZir Ofc <theyaemoribot@gmail.com>
2024-09-26 22:19:02 -0500

    © ⍴᥆ᥕᥱrᥱძ ᑲᥡ sᥙᥒᥣіgһ𝗍 𝗍ᥱᥲm ❀

--- plugins/convertidor-tovideo.js
+++ plugins/convertidor-tovideo.js
@@ -9,7 +9,7 @@ const handler = async (m, {conn, usedPrefix, command}) => {
   conn.reply(m.chat, wait, m, {
   contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
   title: packname,
-  body: wm,
+  body: dev,
   previewType: 0, thumbnail: icons,
   sourceUrl: channel }}})
   if (/webp/.test(mime)) {
