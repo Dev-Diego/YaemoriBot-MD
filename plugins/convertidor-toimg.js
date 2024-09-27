2ddd048909a526fd0bf8edc9e9e3e57916a9970b
SheZir Ofc <theyaemoribot@gmail.com>
2024-09-26 22:24:05 -0500

    © ⍴᥆ᥕᥱrᥱძ ᑲᥡ sᥙᥒᥣіgһ𝗍 𝗍ᥱᥲm ❀

--- plugins/convertidor-toimg.js
+++ plugins/convertidor-toimg.js
@@ -10,7 +10,7 @@ if (!/sticker/.test(mime)) throw notStickerMessage
 conn.reply(m.chat, wait, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
 title: packname,
-body: wm,
+body: dev,
 previewType: 0, thumbnail: icons,
 sourceUrl: channel }}})
 let media = await q.download()
