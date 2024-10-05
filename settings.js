95fd1f33d3efbd0d27a6bdd08803091cf27478e9
SheZir Ofc <theyaemoribot@gmail.com>
2024-10-05 11:11:57 -0500

    © ⍴᥆ᥕᥱrᥱძ ᑲᥡ sᥙᥒᥣіgһ𝗍 𝗍ᥱᥲm ❀

--- settings.js
+++ settings.js
@@ -73,7 +73,7 @@ global.channel = 'https://whatsapp.com/channel/0029VaQD7LAJP216tu9liI2A'
 global.channel2 = 'https://whatsapp.com/channel/0029Vam7yUg77qVaz3sIAp0z'
 global.md = 'https://github.com/Dev-Diego/YaemoriBot-MD' 
 global.yt = 'https://www.youtube.com/@OfcDiego'
-global.tiktok = 'https://tiktok.com/@theyaemoribot'
+global.tiktok = 'https://tiktok.com/@dev_diego'
 global.correo = 'theyaemoribot@gmail.com'
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
