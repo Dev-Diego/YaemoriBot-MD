f6713ff6d71d9c5faa8118a434562f0302392b9e
SheZir Ofc <theyaemoribot@gmail.com>
2024-10-05 23:58:46 -0500

    © ⍴᥆ᥕᥱrᥱძ ᑲᥡ sᥙᥒᥣіgһ𝗍 𝗍ᥱᥲm ❀

--- plugins/owner-delprem.js
+++ plugins/owner-delprem.js
@@ -3,9 +3,9 @@ const handler = async (m, {conn, text, usedPrefix, command}) => {
   if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false;
   else who = m.chat;
   const user = global.db.data.users[who];
-  if (!who) throw `🏓 Ingresa un @tag el que quiera quitarle el premium`;
-  if (!user) throw `*☘️ Ese humano no está en mi base de datos*`;
-  if (user.premiumTime = 0) throw '*🥷 El humano no es usuario premium 👑*';
+  if (!who) return conn.reply(m chat, `🏓 Ingresa un @tag el que quiera quitarle el premium`, m);
+  if (!user) return conn.reply(m.chat, `*☘️ Ese humano no está en mi base de datos*`, m);
+  if (user.premiumTime = 0) return conn.reply(m.chat, '*🥷 El humano no es usuario premium 👑*', m);
   const txt = text.replace('@' + who.split`@`[0], '').trim();
 
   user.premiumTime = 0;
@@ -15,7 +15,7 @@ const handler = async (m, {conn, text, usedPrefix, command}) => {
   const textdelprem = `*@${who.split`@`[0]} ya no es usuario premium 👑*`;
   m.reply(textdelprem, null, {mentions: conn.parseMention(textdelprem)});
 };
-handler.help = ['delprem <@user>'];
+handler.help = ['delprem'];
 handler.tags = ['owner'];
 handler.command = ['remove', 'delpremium']
 handler.group = true;
