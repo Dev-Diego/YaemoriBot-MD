d9006a2ae09fe5df9dbfb3cc578877f67bd1ccc3
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-10-06 18:18:21 -0500

    © ⍴᥆ᥕᥱrᥱძ ᑲᥡ sᥙᥒᥣіgһ𝗍 𝗍ᥱᥲm ❀

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const require = createRequire(__dirname);
 const { name, description, author, version } = require(join(__dirname, './package.json'));
 const { say } = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
-say("YaemoriBot - MD", {
+say('YaemoriBot - MD', {
 font: 'tiny',
 align: 'center',
 colors: ['#ff8000'],
