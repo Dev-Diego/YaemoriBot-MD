c961d23ecd3788197b593f16608877bbb004264b
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-10-06 10:28:09 -0500

    © ⍴᥆ᥕᥱrᥱძ ᑲᥡ sᥙᥒᥣіgһ𝗍 𝗍ᥱᥲm ❀

--- plugins/owner-delprem.js
+++ plugins/owner-delprem.js
@@ -3,9 +3,9 @@ const handler = async (m, {conn, text, usedPrefix, command}) => {
   if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false;
   else who = m.chat;
   const user = global.db.data.users[who];
-  if (!who) return conn.reply(m chat, `🏓 Ingresa un @tag el que quiera quitarle el premium`, m);
-  if (!user) return conn.reply(m.chat, `*☘️ Ese humano no está en mi base de datos*`, m);
-  if (user.premiumTime = 0) return conn.reply(m.chat, `*🥷 El humano no es usuario premium 👑*`, m);
+  if (!who) m.reply(`🏓 Ingresa un @tag el que quiera quitarle el premium`, m);
+  if (!user).reply(`*☘️ Ese humano no está en mi base de datos*`, m);
+  if (user.premiumTime = 0) m.reply(`*🥷 El humano no es usuario premium 👑*`, m);
   const txt = text.replace('@' + who.split`@`[0], '').trim();
 
   user.premiumTime = 0;
