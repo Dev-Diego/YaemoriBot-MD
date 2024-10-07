11bf0019e75124a013d1e119b22fea2b28e73a16
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-10-07 13:02:21 -0500

    © ⍴᥆ᥕᥱrᥱძ ᑲᥡ sᥙᥒᥣіgһ𝗍 𝗍ᥱᥲm ❀

--- handler.js
+++ handler.js
@@ -276,19 +276,15 @@ typeof plugin.command === 'string' ?
 plugin.command === command :
 false
 
-if (!isAccept) {
+if (!isAccept)
 continue
-}
 m.plugin = name
 if (m.chat in global.db.data.chats || m.sender in global.db.data.users) {
 let chat = global.db.data.chats[m.chat]
 let user = global.db.data.users[m.sender]
-if (name != 'Grupo•unbanchat.js' && chat?.isBanned) && name != 'owner-exec2.js' && name != 'tool-delete.js' && chat?.isBanned && !isROwner) return 
-if (m.text && user.banned && !isROwner) {
-if (user.antispam > 2) return
-m.reply(`🚫 Está baneado(a), no puede usar los comandos de este bot!\n\n${user.bannedReason ? `\n💌 *Motivo:* 
-${user.bannedReason}` : '💌 *Motivo:* Sin Especificar'}\n\n⚠️ *Si este bot es cuenta oficial y tiene evidencia que respalde que este mensaje es un error, puede exponer su caso en:*\n\n🤍 ${asistencia}`)
-user.antispam++        
+if (name != 'Grupo•unbanchat.js' && chat?.isBanned)
+return
+if (name != 'Owner•unbanuser.js' && user?.banned)
 return
 }
 
