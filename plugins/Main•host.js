aef1551e539d049952d0252265d2ebf355d65acb
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-10-10 18:26:27 -0500

    © ⍴᥆ᥕᥱrᥱძ ᑲᥡ sᥙᥒᥣіgһ𝗍 𝗍ᥱᥲm ❀

--- plugins/Main•host.js
+++ plugins/Main•host.js
@@ -1,38 +1,18 @@
 let handler = async (m, { conn, usedPrefix, command, text }) => {
 
-let txt = `*¿Tu Nokia es muy lento y necesitas que tu bot esté activo 24/7?* 📱⏳
+let txt = `🌟 *Sky Ultra Plus: Tu Solución de Hosting de Alto Rendimiento* 🌟
 
-¡Tenemos la solución perfecta para ti! 🎉 Mantén tu bot funcionando sin interrupciones con nuestros servidores, Ofrecemos servidores gratuitos y de pago a precios súper accesibles, al alcance de todos. 💸 
+🚀 *¿Buscas un hosting rápido y confiable?* Sky Ultra Plus es tu opción ideal. Ofrecemos servidores de alto rendimiento a precios accesibles, perfectos para cualquier proyecto.
 
-🖥️ *Totalmente compatible con YaemoriBot:* Disfruta al máximo de su potencial en nuestros servidores de alto rendimiento, asegurando una experiencia fluida y de alta calidad. El staff de YaemoriBot y SkyUltraPlus Host se encarga de que disfrutes de todas sus funciones al máximo. 😺✨
+*Beneficios de Sky Ultra Plus:*
+⚡• *Velocidad Ultrarápida:* Tu proyecto responderá en segundos.
+🔒 • *Seguridad total:* Protección avanzada.
+📞 • *Soporte 24/7:* Estamos aquí para ayudarte en cualquier momento.
 
-🟢 \`\`\`Información del Host\`\`\`
-
-💻 *Página:*
+🌐 *Página:*
 https://dash.skyultraplus.com
 
-*🟢 Dashboard:*
-https://dash.skyultraplus.com
-
-⚙️ *Panel*
-https://panel.skyultraplus.com
-
-💥 *Comunidad de WhatsApp:*
-https://chat.whatsapp.com/KGPhTIfgOzZCMNqoc3R7OW
-
-*🟣 Discord:*
-https://discord.gg/zvKgtc2RBc
-
-🧡 *Canal de WhatsApp:*
-https://whatsapp.com/channel/0029VakUvreFHWpyWUr4Jr0g
-
-🗣📲 *Contacto:*
-• wa.me/5214531287294
-• wa.me/15167096032
-• wa.me/573147616444
-• https://www.facebook.com/elrebelde21
-
-No esperes más y lleva tu bot al siguiente nivel con nuestro servicio de alojamiento. ¡Es fácil, rápido y económico! 💪🚀` 
+¡Elige Sky Ultra Plus y lleva tu presencia online al siguiente nivel! 🌟🚀💻` 
 
 await conn.sendMessage(m.chat, { text: txt,
 contextInfo:{
