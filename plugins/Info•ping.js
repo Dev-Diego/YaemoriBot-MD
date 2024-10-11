114d038a0f312272234cb571157c85b99a7323a2
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-10-11 10:25:11 -0500

    © ⍴᥆ᥕᥱrᥱძ ᑲᥡ sᥙᥒᥣіgһ𝗍 𝗍ᥱᥲm ❀

--- plugins/Info•ping.js
+++ plugins/Info•ping.js
@@ -2,14 +2,20 @@ import speed from 'performance-now'
 import { spawn, exec, execSync } from 'child_process'
 
 let handler = async (m, { conn }) => {
-         let timestamp = speed();
-         let latensi = speed() - timestamp;
-         exec(`neofetch --stdout`, (error, stdout, stderr) => {
-          let child = stdout.toString("utf-8");
-          let ssd = child.replace(/Memory:/, "Ram:");
+let timestamp = speed()
+let latensi = speed() - timestamp;
+exec(`neofetch --stdout`, (error, stdout, stderr) => {
+let child = stdout.toString("utf-8");
+let ssd = child.replace(/Memory:/, "Ram:")
 
-          conn.reply(m.chat, `*Pong* 🏓 ${latensi.toFixed(4)} ms`, m, rcanal);
-            });
+const { key } = await conn.sendMessage(m.chat, {text: `🚀 Ping.....`}, {quoted: m})
+await delay(1000 * 1)
+await conn.sendMessage(m.chat, {text: `🚀 Pong.....`, edit: key})
+await delay(1000 * 1)
+await conn.sendMessage(m.chat, {text: `♻️ Cargando...`, edit: key})
+await conn.sendMessage(m.chat, {text: `*Pong* 🏓 ${latensi.toFixed(4)} ms`, edit: key})
+
+})
 }
 handler.help = ['ping']
 handler.tags = ['info']
