d4bb952907ab44e6f11d787a658ba15229a0d28e
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-10-12 17:20:16 -0500

    © ⍴᥆ᥕᥱrᥱძ ᑲᥡ sᥙᥒᥣіgһ𝗍 𝗍ᥱᥲm ❀

--- plugins/Main•menu.js
+++ plugins/Main•menu.js
@@ -15,7 +15,7 @@ let handler = async (m, { conn, usedPrefix, text, args, command }) => {
     let name = conn.getName(m.sender)
     let api = await axios.get(`https://deliriusapi-official.vercel.app/tools/country?text=${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}`)
    let userNationalityData = api.data.result
-   let userNationality = userNationalityData ? `${userNationalityData.name} ${userNationalityData.emoji}` : 'Desconocido'
+   let userNationality = userNationalityData ? `${userNationalityData.name}` : 'Desconocido'
     let locale = 'es';
     let d = new Date(new Date + 3600000);
     let time = d.toLocaleTimeString(locale, {
