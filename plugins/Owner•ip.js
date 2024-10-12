070e7c9868a3df9e2bf7bb8cf31c74f1e379c865
SheZir Ofc <theyaemoribot@gmail.com>
2024-10-12 13:24:52 -0500

    © ⍴᥆ᥕᥱrᥱძ ᑲᥡ sᥙᥒᥣіgһ𝗍 𝗍ᥱᥲm ❀

--- plugins/Owner•ip.js
+++ plugins/Owner•ip.js
@@ -6,48 +6,49 @@ let bot = '🧑🏻‍💻 Buscando....'
 conn.reply(m.chat, bot, m, rcanal, )
   if (!text) return conn.reply(m.chat, '🚩 *Te Faltó La <Ip>*', m, rcanal, )
 
-  axios.get(`https://ipwho.is/${text}`).then ((res) => {
-    const data = res.data
+  let res = await fetch(`https://ipwho.is/${text}`).then(result => result.json());
 
       if (String(data.status) !== "success") {
         throw new Error(data.message || "Falló")
       }
-    let ipsearch = `
+const formatIPInfo = (info) => {
+ return `
 > ✰ *IP INFORMACION*
-• IP: ${data.ip || 'N/A'}
-• EXITO: ${data.success || 'N/A'}
-• TIPO: ${data.type || 'N/A'}
-• CONTINENTE: ${data.continent || 'N/A'}
-• CODIGO DEL CONTIENE: ${data.continent_code || 'N/A'}
-• PAIS: ${data.country || 'N/A'}
-• CODIGO DEL PAIS: ${data.country_code || 'N/A'}
-• REGION: ${data.region || 'N/A'}
-• CODIGO DE REGION: ${data.region_code || 'N/A'}
-• CIUDAD: ${data.city || 'N/A'}
-• LATITUD: ${data.latitude || 'N/A'}
-• LONGITUD: ${data.longitude || 'N/A'}
-• ES EU: ${data.is_eu ? 'Yes' : 'No'}
-• POSTAL: ${data.postal || 'N/A'}
+• IP: ${info.ip || 'N/A'}
+• EXITO: ${info.success || 'N/A'}
+• TIPO: ${info.type || 'N/A'}
+• CONTINENTE: ${info.continent || 'N/A'}
+• CODIGO DEL CONTIENE: ${info.continent_code || 'N/A'}
+• PAIS: ${info.country || 'N/A'}
+• CODIGO DEL PAIS: ${info.country_code || 'N/A'}
+• REGION: ${info.region || 'N/A'}
+• CODIGO DE REGION: ${info.region_code || 'N/A'}
+• CIUDAD: ${info.city || 'N/A'}
+• LATITUD: ${info.latitude || 'N/A'}
+• LONGITUD: ${info.longitude || 'N/A'}
+• ES EU: ${info.is_eu ? 'Yes' : 'No'}
+• POSTAL: ${info.postal || 'N/A'}
 • CODIGO DE TELEFONO: ${info.calling_code || 'N/A'}
-• CAPITAL: ${data.capital || 'N/A'}
-• BORDES: ${data.borders || 'N/A'}
+• CAPITAL: ${info.capital || 'N/A'}
+• BORDES: ${info.borders || 'N/A'}
 • BANDERA:
- - IMAGEN: ${data.flag?.img || 'N/A'}
- - EMOJI: ${data.flag?.emoji || 'N/A'}
- - EMOJI UNICODE: ${data.flag?.emoji_unicode || 'N/A'}
+ - IMAGEN: ${info.flag?.img || 'N/A'}
+ - EMOJI: ${info.flag?.emoji || 'N/A'}
+ - EMOJI UNICODE: ${info.flag?.emoji_unicode || 'N/A'}
 • CONEXION:
- - ASN: ${data.connection?.asn || 'N/A'}
- - ORGANIZACION: ${data.connection?.org || 'N/A'}
- - ISP: ${data.connection?.isp || 'N/A'}
- - DOMINIO: ${data.connection?.domain || 'N/A'}
+ - ASN: ${info.connection?.asn || 'N/A'}
+ - ORGANIZACION: ${info.connection?.org || 'N/A'}
+ - ISP: ${info.connection?.isp || 'N/A'}
+ - DOMINIO: ${info.connection?.domain || 'N/A'}
 • HORARIO:
- - ID: ${data.timezone?.id || 'N/A'}
- - ABREVIACION: ${data.timezone?.abbr || 'N/A'}
- - ES DST: ${data.timezone?.is_dst ? 'Yes' : 'No'}
- - SET APAGADO: ${data.timezone?.offset || 'N/A'}
- - UTC: ${data.timezone?.utc || 'N/A'}
- - TIEMPO: ${data.timezone?.current_time || 'N/A'}
-`.trim()
+ - ID: ${info.timezone?.id || 'N/A'}
+ - ABREVIACION: ${info.timezone?.abbr || 'N/A'}
+ - ES DST: ${info.timezone?.is_dst ? 'Yes' : 'No'}
+ - SET APAGADO: ${info.timezone?.offset || 'N/A'}
+ - UTC: ${info.timezone?.utc || 'N/A'}
+ - TIEMPO: ${info.timezone?.current_time || 'N/A'}
+`;
+};
 
 conn.reply(m.chat, ipsearch, m, rcanal, )
 })
