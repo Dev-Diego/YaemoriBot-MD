c15c6fa65e1b4badf6415a8db2c17ffa3614e939
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-10-12 12:25:45 -0500

    © ⍴᥆ᥕᥱrᥱძ ᑲᥡ sᥙᥒᥣіgһ𝗍 𝗍ᥱᥲm ❀

--- plugins/Owner•autoadmin.js
+++ plugins/Owner•autoadmin.js
@@ -15,6 +15,5 @@ handler.help = ['autoadmin'];
 handler.command = ['autoadmin'];
 handler.rowner = true;
 handler.group = true;
-handler.private = true;
 handler.botAdmin = true;
 export default handler;
