ddc4e9ed513c072a30a1fe72d603184fe90de930
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-10-12 21:28:13 -0500

    © ⍴᥆ᥕᥱrᥱძ ᑲᥡ sᥙᥒᥣіgһ𝗍 𝗍ᥱᥲm ❀

---
+++ plugins/Herramientas•idchannel.js
@@ -0,0 +1,28 @@
+const { Client, LocalAuth } = require('whatsapp-web.js');
+
+const client = new Client({
+    authStrategy: new LocalAuth()
+});
+
+client.on('qr', (qr) => {
+    console.log('QR RECEIVED', qr);
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', async msg => {
+    if (msg.body === '!channelid') {
+        const chat = await msg.getChat();
+        const channelId = chat.id._serialized;
+
+        msg.reply(`La ID de este canal es: ${channelId}`);
+    }
+});
+
+handler.help = ['idchannel']
+handler.tags = ['tools']
+handler.command = ['id', 'idchannel']
+handler.register = true 
+client.initialize();
