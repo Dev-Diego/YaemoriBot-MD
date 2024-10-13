7fde572d27f4b1d54bcea25feef9dad22f6ed6ab
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-10-12 19:46:00 -0500

    © ⍴᥆ᥕᥱrᥱძ ᑲᥡ sᥙᥒᥣіgһ𝗍 𝗍ᥱᥲm ❀

--- plugins/Rpg•cartera.js
+++ plugins/Rpg•cartera.js
@@ -3,13 +3,11 @@ let who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m
 if (who == conn.user.jid) return error 
 if (!(who in global.db.data.users)) return conn.reply(m.chat, '🍭 El usuario no se encuentra en mi base de Datos.', m, fake)
 const name = conn.getName(who);
-let txt = `*🐢 Balance de ${name}*
-
-	➺ *Galletas* : ${global.db.data.users[who].cookies} 
-	➺ *Banco* : ${global.db.data.users[who].bank} 
-	➺ *Experiencia* : ${global.db.data.users[who].exp}
-
-> Para proteger tus *Galletas*, depósitalas en el banco usando *#depositar*`
+let txt = `*🌴 Balance de ${name}*\n\n`
+txt +=	`➺ *Galletas* : ${global.db.data.users[who].cookies}\n` 
+txt +=	`➺ *Banco* : ${global.db.data.users[who].bank}\n` 
+txt +=	`➺ *Experiencia* : ${global.db.data.users[who].exp}\n\n`
+txt += `> Para proteger tus *Galletas*, depósitalas en el banco usando *#depositar*`
 await conn.reply(m.chat, txt, m, rcanal)
 }
 
