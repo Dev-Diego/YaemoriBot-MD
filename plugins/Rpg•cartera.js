9046763f21aa94673807c33209464c28854f64c2
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-10-12 19:35:27 -0500

    © ⍴᥆ᥕᥱrᥱძ ᑲᥡ sᥙᥒᥣіgһ𝗍 𝗍ᥱᥲm ❀

--- plugins/Rpg•cartera.js
+++ plugins/Rpg•cartera.js
@@ -1,17 +1,18 @@
 let handler = async (m, {conn, usedPrefix}) => {
 let who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m.sender
-let user = global.db.data.users[m.sender]
 user.bank -= count * 1
 if (who == conn.user.jid) return error 
 if (!(who in global.db.data.users)) return conn.reply(m.chat, '🍭 El usuario no se encuentra en mi base de Datos.', m, fake)
 let user = global.db.data.users[who]
-await conn.reply(m.chat, `*🌵 Balance de ${user}*
+let txt = `*🌵 Balance de ${user}*
 
 	➠ *Galletas* : ${user.cookies}
 	➠ *Banco* : ${user.bank}
 	➠ *Experiencia* : ${global.db.data.users[who].exp}
 
-> Para proteger tus *Galletas*, depósitalas en el banco usando *#depositar*`, m, rcanal)}
+> Para proteger tus *Galletas*, depósitalas en el banco usando *#depositar*`
+await conn.reply(m.chat, txt, m, rcanal)
+}
 
 handler.help = ['cookies']
 handler.tags = ['rpg']
