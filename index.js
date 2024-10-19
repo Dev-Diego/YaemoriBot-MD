349ea3c3b0162b4b275b4d95cfd43e505b1c8ce1
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-10-19 09:25:26 -0500

    © ⍴᥆ᥕᥱrᥱძ ᑲᥡ sᥙᥒᥣіgһ𝗍 𝗍ᥱᥲm ❀

--- index.js
+++ index.js
@@ -33,10 +33,11 @@ say(description, {
   gradient: ['blue', 'magenta']
 });
 
-const message = `${subtitleStyle('Desarrollado por »')} ${responseStyle(author.name)}
+const message = `
+${subtitleStyle('Desarrollado por »')} ${responseStyle(author.name)}
 ${subtitleStyle('Código basado por »')} ${responseStyle('BrunoSobrino')}
 ${subtitleStyle('Colaboradores activos »')} ${responseStyle(activeCollaborators)}
-${subtitleStyle('Versión »')} ${responseStyle(version)}`
+${subtitleStyle('Versión »')} ${responseStyle(version)}`;
 
 console.log(boxen(message, {
   padding: 1,
