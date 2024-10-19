a831ad655808e8c95add6ced0a9150c8e2848a74
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-10-19 09:31:37 -0500

    © ⍴᥆ᥕᥱrᥱძ ᑲᥡ sᥙᥒᥣіgһ𝗍 𝗍ᥱᥲm ❀

--- index.js
+++ index.js
@@ -1,107 +1,84 @@
-import { join, dirname } from 'path';
-import { createRequire } from 'module';
-import { fileURLToPath } from 'url';
-import boxen from 'boxen';
+import { join, dirname } from 'path'
+import { createRequire } from 'module'
+import { fileURLToPath } from 'url'
+import boxen from 'boxen'
 import { setupMaster, fork } from 'cluster';
 import { watchFile, unwatchFile } from 'fs';
-import cfonts from 'cfonts';
-import { createInterface } from 'readline';
-import yargs from 'yargs';
-import chalk from 'chalk';
+import cfonts from 'cfonts'
+import { createInterface } from 'readline'
+import yargs from 'yargs'
+import chalk from 'chalk'
+console.log('\n✰ Iniciando Yaemori ✰')
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const require = createRequire(__dirname)
+const { name, description, collaborators, author, version } = require(join(__dirname, './package.json'))
+const { say } = cfonts
+const rl = createInterface(process.stdin, process.stdout)
+const subtitleStyle = chalk.white.bold
+const responseStyle = chalk.dim.bold
 
-console.log('\n✰ Iniciando Yaemori ✰');
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-const require = createRequire(__dirname);
-const { name, description, collaborators, author, version } = require(join(__dirname, './package.json'));
-const { say } = cfonts;
-const rl = createInterface(process.stdin, process.stdout);
-
-const subtitleStyle = chalk.white.bold;
-const responseStyle = chalk.dim.bold;
-
-let activeCollaborators = Object.values(collaborators).join(', ');
-
-say('yaemori\nbot md', {
-  align: 'center',
-  gradient: ['red', 'blue']
-});
-
-say(description, {
-  font: 'console',
-  align: 'center',
-  gradient: ['blue', 'magenta']
-});
-
-const message = `
-${subtitleStyle('Desarrollado por »')} ${responseStyle(author.name)}
+let activeCollaborators = ''
+for (const key in collaborators) {
+if (collaborators.hasOwnProperty(key)) {
+activeCollaborators += collaborators[key] + ', '
+}}
+activeCollaborators = activeCollaborators.slice(0, -2);
+cfonts.say('yaemori\nbot md', {
+align: 'center',           
+gradient: ['red', 'blue'] 
+})
+cfonts.say(description, {
+font: 'console',
+align: 'center',
+gradient: ['blue', 'magenta']
+})
+const message = `${subtitleStyle('Desarrollado por »')} ${responseStyle(author.name)}
 ${subtitleStyle('Código basado por »')} ${responseStyle('BrunoSobrino')}
 ${subtitleStyle('Colaboradores activos »')} ${responseStyle(activeCollaborators)}
-${subtitleStyle('Versión »')} ${responseStyle(version)}`;
-
-console.log(boxen(message, {
-  padding: 1,
-  margin: 1,
-  borderStyle: 'double',
-  borderColor: 'blue',
-  float: 'center',
-}));
-
-let isRunning = false;
-
+${subtitleStyle('Versión »')} ${responseStyle(version)}`
+console.log(boxen(message, { padding: 1, margin: 1, borderStyle: 'double', borderColor: 'blue', float: 'center', }))
+var isRunning = false
 function start(file) {
-  if (isRunning) return;
-  isRunning = true;
-
-  const args = [join(__dirname, file), ...process.argv.slice(2)];
-
-  setupMaster({
-    exec: args[0],
-    args: args.slice(1),
-  });
-
-  const p = fork();
-
-  p.on('message', data => {
-    switch (data) {
-      case 'reset':
-        p.process.kill();
-        isRunning = false;
-        start.apply(this, arguments);
-        break;
-      case 'uptime':
-        p.send(process.uptime());
-        break;
-    }
-  });
-
-  p.on('exit', (code) => {
-    isRunning = false;
-    if (code !== 0) {
-      console.error('Error:\n', code);
-      process.exit();
-
-      watchFile(args[0], () => {
-        unwatchFile(args[0]);
-        start(file);
-      });
-    }
-  });
-
-  const opts = yargs(process.argv.slice(2)).exitProcess(false).parse();
-
-  if (!opts.test && !rl.listenerCount('line')) {
-    rl.on('line', line => {
-      p.emit('message', line.trim());
-    });
-  }
+if (isRunning) return
+isRunning = true
+let args = [join(__dirname, file), ...process.argv.slice(2)]
+setupMaster({
+exec: args[0],
+args: args.slice(1),
+})
+let p = fork()
+p.on('message', data => {
+switch (data) {
+case 'reset':
+p.process.kill()
+isRunning = false
+start.apply(this, arguments)
+break
+case 'uptime':
+p.send(process.uptime())
+break
 }
-
+})
+p.on('exit', (_, code) => {
+isRunning = false
+console.error('🚩 Error:\n', code)
+process.exit()
+if (code === 0) return
+watchFile(args[0], () => {
+unwatchFile(args[0])
+start(file)
+})
+})
+let opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
+if (!opts['test'])
+if (!rl.listenerCount()) rl.on('line', line => {
+p.emit('message', line.trim())
+})
+}
 process.on('warning', (warning) => {
-  if (warning.name === 'MaxListenersExceededWarning') {
-    console.warn('🚩 Se excedió el límite de Listeners en:');
-    console.warn(warning.stack);
-  }
-});
-
-start('sunlight.js');
+if (warning.name === 'MaxListenersExceededWarning') {
+console.warn('🚩 Se excedió el límite de Listeners en:')
+console.warn(warning.stack)
+}
+})
+start('sunlight.js')
