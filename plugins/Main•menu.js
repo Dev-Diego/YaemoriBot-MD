e2632a1b564716c607ce17bd5e6ec4667528f430
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-10-20 14:22:07 -0500

    © ⍴᥆ᥕᥱrᥱძ ᑲᥡ sᥙᥒᥣіgһ𝗍 𝗍ᥱᥲm ❀

--- plugins/Main•menu.js
+++ plugins/Main•menu.js
@@ -38,6 +38,7 @@ let handler = async (m, { conn, usedPrefix, text, args, command }) => {
 txt+= '.͜°˖ `ᴄʀᴇᴀᴅᴏʀ ::`' + ` DevDiego\n`;
 txt+= '.͜°˖ `ʙᴏᴛ ::`' + ` YaemoriBot-MD\n`;
 txt+= '.͜°˖ `ꜰᴇᴄʜᴀ ::`' + ` ${moment.tz('America/Bogota').format('DD/MM/YY')}\n`;
+txt+= '.͜°˖ `ᴅɪᴀ ::`' + ` ${dia}\n`;
 txt+= '.͜°˖ `ᴘᴀɪs ::`' + ` ${userNationality}\n`;
 txt+= '.͜°˖ `ᴘʀᴇꜰɪᴊᴏ ::`' + ` 「 ${usedPrefix} 」\n`;
 txt+= '.͜°˖ `ᴜꜱᴜᴀʀɪᴏꜱ ::`' + ` ${rtotal}\n`;
