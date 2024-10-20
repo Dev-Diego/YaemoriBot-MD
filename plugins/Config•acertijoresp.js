c52ca5d40f969aad1cf98b8413ec503aed471948
SheZir Ofc <theyaemoribot@gmail.com>
2024-10-20 10:24:43 -0500

    © ⍴᥆ᥕᥱrᥱძ ᑲᥡ sᥙᥒᥣіgһ𝗍 𝗍ᥱᥲm ❀

--- plugins/Config•acertijoresp.js
+++ plugins/Config•acertijoresp.js
@@ -10,11 +10,11 @@ handler.before = async function(m) {
     const json = JSON.parse(JSON.stringify(this.tekateki[id][1]));
     if (m.text.toLowerCase() == json.response.toLowerCase().trim()) {
       global.db.data.users[m.sender].estrellas += this.tekateki[id][2];
-      m.reply(`🤍 *Respuesta correcta!*\n+${this.tekateki[id][2]} Centavos`);
+      m.reply(`🌵 *Respuesta correcta!*\n+${this.tekateki[id][2]} Galletas`);
       clearTimeout(this.tekateki[id][3]);
       delete this.tekateki[id];
-    } else if (similarity(m.text.toLowerCase(), json.response.toLowerCase().trim()) >= threshold) m.reply(`Casi lo logras!`);
-    else m.reply('Respuesta incorrecta!');
+    } else if (similarity(m.text.toLowerCase(), json.response.toLowerCase().trim()) >= threshold) m.reply(`🚩 Casi lo logras!`);
+    else m.reply('🍭 Respuesta incorrecta!');
   }
   return !0;
 };
