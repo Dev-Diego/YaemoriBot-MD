dcf0bfaa525bbf49c2c4c2fc72f58994cfd9452b
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-10-20 09:42:07 -0500

    © ⍴᥆ᥕᥱrᥱძ ᑲᥡ sᥙᥒᥣіgһ𝗍 𝗍ᥱᥲm ❀

--- plugins/Game•ppt.js
+++ plugins/Game•ppt.js
@@ -6,8 +6,8 @@ const handler = async (m, {conn, text, command, usedPrefix, args}) => {
   const time = global.db.data.users[m.sender].wait + 10000;
   if (new Date - global.db.data.users[m.sender].wait < 10000) throw `*🕓 Tendrás que esperar ${Math.floor((time - new Date()) / 1000)} segundos antes de poder volver a jugar*`;
 
-//  conn.reply(m.chat, `*𝐏𝐢𝐞𝐝𝐫𝐚 🗿, 𝐏𝐚𝐩𝐞𝐥 📄 𝐨 𝐓𝐢𝐣𝐞𝐫𝐚 ✂️*\n\n*—◉ 𝚙𝚞𝚎𝚍𝚎𝚜 𝚞𝚜𝚊𝚛 𝚎𝚜𝚝𝚘𝚜 𝚌𝚘𝚖𝚊𝚗𝚍𝚘𝚜:*\n*◉ ${usedPrefix + command} piedra*\n*◉ ${usedPrefix + command} papel*\n*◉ ${usedPrefix + command} tijera*`, m);
- if (!args[0]) return conn.sendButton(m.chat, `*𝐏𝐢𝐞𝐝𝐫𝐚 🗿, 𝐏𝐚𝐩𝐞𝐥 📄 𝐨 𝐓𝐢𝐣𝐞𝐫𝐚 ✂️*\n\n*—◉  𝙿𝚎𝚍𝚎𝚜 𝚞𝚜𝚊𝚛 𝚕𝚘𝚜 𝚋𝚘𝚝𝚘𝚗𝚎𝚜 𝚙𝚊𝚛𝚊 𝚓𝚞𝚐𝚊𝚛 𝚘 𝚝𝚊𝚖𝚋𝚒𝚎𝚗 𝚙𝚞𝚎𝚍𝚎𝚜 𝚞𝚜𝚊𝚛 𝚎𝚜𝚝𝚘𝚜 𝚌𝚘𝚖𝚊𝚗𝚍𝚘𝚜:*\n*◉ ${usedPrefix + command} piedra*\n*◉ ${usedPrefix + command} papel*\n*◉ ${usedPrefix + command} tijera*`, wm, pp, [['Piedra 🗿', `${usedPrefix + command} piedra`], ['Papel 📄', `${usedPrefix + command} papel`], ['Tijera ✂️', `${usedPrefix + command} tijera`]], m)
+conn.reply(m.chat, `*Piedra 🗿, Papel 📄 o Tijera ✂️*\n\n*🌵 Puedes usar estos comandos:*\n*• ${usedPrefix + command} piedra*\n*• ${usedPrefix + command} papel*\n*• ${usedPrefix + command} tijera*`, m);
+ if (!args[0]) return 
   let astro = Math.random();
   if (astro < 0.34) {
     astro = 'piedra';
@@ -19,14 +19,14 @@ const handler = async (m, {conn, text, command, usedPrefix, args}) => {
   const textm = text.toLowerCase();
   if (textm == astro) {
     global.db.data.users[m.sender].cookies += 10;
-    m.reply(`*🔰 Empate!*\n\n*👉🏻 Tu: ${textm}*\n*👉🏻 El Bot: ${astro}*\n*🎁 Premio +10 Cookies*`);
+    m.reply(`*🌵 Empate!*\n\n*👉🏻 Tu: ${textm}*\n*👉🏻 El Bot: ${astro}*\n*🎁 Premio +10 Cookies*`);
   } else if (text == 'papel') {
     if (astro == 'piedra') {
       global.db.data.users[m.sender].cookies += 50;
       m.reply(`*🥳 Tú ganas! 🎉*\n\n*👉🏻 Tu: ${textm}*\n*👉🏻 El Bot: ${astro}*\n*🎁 Premio +50 Cookies*`);
     } else {
       global.db.data.users[m.sender].Cookies -= 30;
-      m.reply(`*☠️ Tú pierdes! ❌*\n\n*👉🏻 Tu: ${textm}*\n*👉🏻 El Bot: ${astro}*\n*❌ Premio -30 Cookies*`);
+      m.reply(`*🌵 ¡Tú pierdes! 🌵*\n\n*👉🏻 Tu: ${textm}*\n*👉🏻 El Bot: ${astro}*\n*❌ Premio -30 Cookies*`);
     }
   } else if (text == 'tijera') {
     if (astro == 'papel') {
@@ -34,7 +34,7 @@ const handler = async (m, {conn, text, command, usedPrefix, args}) => {
       m.reply(`*🥳 Tú ganas! 🎉*\n\n*👉🏻 Tu: ${textm}*\n*👉🏻 El Bot: ${astro}*\n*🎁 Premio +50 Cookies*`);
     } else {
       global.db.data.users[m.sender].Cookies -= 30;
-      m.reply(`*☠️ Tú pierdes! ❌*\n\n*👉🏻 Tu: ${textm}*\n*👉🏻 El Bot: ${astro}*\n*❌ Premio -30 Cookies*`);
+      m.reply(`*🌵 ¡Tú pierdes! 🌵*\n\n*👉🏻 Tu: ${textm}*\n*👉🏻 El Bot: ${astro}*\n*❌ Premio -30 Cookies*`);
     }
   } else if (textm == 'tijera') {
     if (astro == 'papel') {
@@ -42,7 +42,7 @@ const handler = async (m, {conn, text, command, usedPrefix, args}) => {
       m.reply(`*🥳 Tú ganas! 🎉*\n\n*👉🏻 Tu: ${textm}*\n*👉🏻 El Bot: ${astro}*\n*🎁 Premio +50 Cookies*`);
     } else {
       global.db.data.users[m.sender].cookies -= 30;
-      m.reply(`*☠️ Tú pierdes! ❌*\n\n*👉🏻 Tu: ${textm}*\n*👉🏻 El Bot: ${astro}*\n*❌ Premio -30 Cookies*`);
+      m.reply(`*🌵 ¡Tú pierdes! 🌵*\n\n*👉🏻 Tu: ${textm}*\n*👉🏻 El Bot: ${astro}*\n*❌ Premio -30 Cookies*`);
     }
   } else if (textm == 'papel') {
     if (astro == 'piedra') {
@@ -50,7 +50,7 @@ const handler = async (m, {conn, text, command, usedPrefix, args}) => {
       m.reply(`*🥳 Tú ganas! 🎉*\n\n*👉🏻 Tu: ${textm}*\n*👉🏻 El Bot: ${astro}*\n*🎁 Premio +50 Cookies*`);
     } else {
       global.db.data.users[m.sender].cookies -= 30;
-      m.reply(`*☠️ Tú pierdes! ❌*\n\n*👉🏻 Tu: ${textm}*\n*👉🏻 El Bot: ${astro}*\n*❌ Premio -30 Cookies*`);
+      m.reply(`*🌵 ¡Tú pierdes! 🌵*\n\n*👉🏻 Tu: ${textm}*\n*👉🏻 El Bot: ${astro}*\n*❌ Premio -30 Cookies*`);
     }
   } else if (textm == 'piedra') {
     if (astro == 'tijera') {
@@ -58,7 +58,7 @@ const handler = async (m, {conn, text, command, usedPrefix, args}) => {
       m.reply(`*🥳 Tú ganas! 🎉*\n\n*👉🏻 Tu: ${textm}*\n*👉🏻 El Bot: ${astro}*\n*🎁 Premio +50 Cookies*`);
     } else {
       global.db.data.users[m.sender].cookies -= 30;
-      m.reply(`*☠️ Tú pierdes! ❌*\n\n*👉🏻 Tu: ${textm}*\n*👉🏻 El Bot: ${astro}*\n*❌ Premio -30 Cookies*`);
+      m.reply(`*🌵 ¡Tú pierdes! 🌵*\n\n*👉🏻 Tu: ${textm}*\n*👉🏻 El Bot: ${astro}*\n*❌ Premio -30 Cookies*`);
     }
   }
   global.db.data.users[m.sender].wait = new Date * 1;
