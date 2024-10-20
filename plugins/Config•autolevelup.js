78be42634f625f23139652011591f3d8a3b9c13e
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-10-20 13:16:17 -0500

    © ⍴᥆ᥕᥱrᥱძ ᑲᥡ sᥙᥒᥣіgһ𝗍 𝗍ᥱᥲm ❀

--- plugins/Config•autolevelup.js
+++ plugins/Config•autolevelup.js
@@ -12,7 +12,7 @@ let before = user.level *
 while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
 global.db.data.users[m.sender].cookies += 5
 if (before !== user.level) {
-m.reply(`*🎉 ¡ F E L I C I D A D E S ! 🎉*\n\n💫 Nivel Actual » *${user.level}*\n🎁 Recompensa » *+5 Galletas*\n📆 Fecha » *fecha: ${moment.tz('America/Bogota').format('DD/MM/YY')}*\n\n> *\`¡Has alcanzado un Nuevo Nivel!\`*
+m.reply(`*🎉 ¡ F E L I C I D A D E S ! 🎉*\n\n💫 Nivel Actual » *${user.level}*\n🎁 Recompensa » *+5 Galletas*\n📆 Fecha » *${moment.tz('America/Bogota').format('DD/MM/YY')}*\n\n> *\`¡Has alcanzado un Nuevo Nivel!\`*
 `.trim())
     }
 } 
