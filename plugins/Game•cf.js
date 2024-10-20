2d905532fc05376c80bacd6ad721b140737fe3d5
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-10-20 09:35:24 -0500

    © ⍴᥆ᥕᥱrᥱძ ᑲᥡ sᥙᥒᥣіgһ𝗍 𝗍ᥱᥲm ❀

---
+++ plugins/Game•cf.js
@@ -0,0 +1,38 @@
+let cooldowns = {}
+
+let handler = async (m, { conn, text, command, usedPrefix }) => {
+    let tiempoEspera = 5
+
+    if (cooldowns[m.sender] && Date.now() - cooldowns[m.sender] < tiempoEspera * 1000) {
+        let tiempoRestante = segundosAHMS(Math.ceil((cooldowns[m.sender] + tiempoEspera * 1000 - Date.now()) / 1000))
+        m.reply(`🌵 Ya has iniciado una apuesta recientemente, espera *⏱ ${tiempoRestante}* para apostar nuevamente.`)
+        return
+    }
+
+    if (!text || !['cara', 'cruz'].includes(text.toLowerCase())) {
+        return conn.reply(m.chat, '🌵 Elige una opción ( *Cara o Cruz* ) para lanzar la moneda.\n\n`» Ejemplo :`\n' + `> *${usedPrefix + command}* cara`, m, rcanal)
+    }
+
+    cooldowns[m.sender] = Date.now()
+    let resultado = Math.random() < 0.5 ? 'cara' : 'cruz'
+    let esGanador = text.toLowerCase() === resultado
+
+    if (esGanador) {
+        global.db.data.users[m.sender].cookies += 1000
+        conn.reply(m.chat, `✿︎ La moneda cayó en *${text}*, acabas de ganar *1000 🍪 Galletas*`, m, rcanal)       
+    } else {
+        global.db.data.users[m.sender].cookies -= 500
+        conn.reply(m.chat, `✿︎ La moneda cayó en *${text}*, acabas de perder *500 🍪 Galletas*`, m, rcanal)
+    }
+}
+
+handler.help = ['cf']
+handler.tags = ['fun']
+handler.command = ['cf', 'flip', 'coinflip']
+handler.register = true
+
+export default handler
+
+function segundosAHMS(segundos) {
+    return `${segundos % 60} segundos`
+}
