6f37944365076a38043206d349d5a9163f9a5e08
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-10-22 07:54:05 -0500

    © ⍴᥆ᥕᥱrᥱძ ᑲᥡ sᥙᥒᥣіgһ𝗍 𝗍ᥱᥲm ❀

--- plugins/Herramientas•inspeccionarChannel.js
+++ plugins/Herramientas•inspeccionarChannel.js
@@ -5,10 +5,12 @@ let handler = async (m, { conn, text }) => {
   const channelUrl = text?.match(/(?:https:\/\/)?(?:www\.)?(?:chat\.|wa\.)?whatsapp\.com\/(?:channel\/|joinchat\/)?([0-9A-Za-z]{22,24})/i)?.[1];
   
   if (!channelUrl) {
+    await m.react(emojis)
     return await conn.reply(m.chat, "*Verifique que sea un enlace de canal de WhatsApp.*", m);
   }
   
   try {
+    await m.react(rwait)
     newsletterInfo = await conn.newsletterMetadata("invite", channelUrl).catch(e => null);
     if (!newsletterInfo) {
       return await conn.reply(m.chat, "*No se encontró información del canal.* Verifique que el enlace sea correcto.", m);
@@ -16,28 +18,11 @@ let handler = async (m, { conn, text }) => {
     
     let caption = "🌵 Id: " + id, newsletterInfo?.preview);
     
-    if (channelUrl ) {
-      await conn.sendMessage(m.chat, {
-        text: caption,
-        contextInfo: {
-          mentionedJid: conn.parseMention(caption),
-          externalAdReply: {
-            title: "Inspector de Canales",
-            body: packname,
-            thumbnailUrl: icons,
-            sourceUrl: text,
-            mediaType: 1,
-            showAdAttribution: false,
-            renderLargerThumbnail: false
-          }
-        }
-      }, { quoted: fkontak });
-      
-      if (newsletterInfo.id) {
-        conn.sendMessage(m.chat, { text: newsletterInfo.id }, { quoted: m });
-      }
-    }
+await conn.reply(m.chat, caption, m, rcanal)
+await m.react(done)
+
   } catch (e) {
+    await m.react(error)
     console.log(e);
   }
 };
