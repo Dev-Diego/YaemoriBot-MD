63eb3b6b948b3694fd11e0c6a0e11c01d2422836
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-10-23 18:20:46 -0500

    © ⍴᥆ᥕᥱrᥱძ ᑲᥡ sᥙᥒᥣіgһ𝗍 𝗍ᥱᥲm ❀

--- plugins/Main•allfake.js
+++ plugins/Main•allfake.js
@@ -66,6 +66,8 @@ global.waittt = '🚀 Cargando...'
 global.waitttt = '🚀 Cargando...'
 
 //Enlaces
+global.skydash = 'https://dash.skyultraplus.com'
+global.skypanel = 'https://panel.skyultraplus.com'
 var canal = 'https://whatsapp.com/channel/0029VaQD7LAJP216tu9liI2A'  
 var canal2 = 'https://whatsapp.com/channel/0029Vam7yUg77qVaz3sIAp0z'
 var git = 'https://github.com/Dev-Diego' 
