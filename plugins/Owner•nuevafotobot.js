88af2f2eb0095b3733bec11664fbbf27d6ebe4c9
SheZir Ofc <theyaemoribot@gmail.com>
2024-10-23 18:49:38 -0500

    © ⍴᥆ᥕᥱrᥱძ ᑲᥡ sᥙᥒᥣіgһ𝗍 𝗍ᥱᥲm ❀

--- plugins/Owner•nuevafotobot.js
+++ plugins/Owner•nuevafotobot.js
@@ -1,6 +1,7 @@
 async function handler(m, { conn: _envio, usedPrefix }) {
 
   try {
+     const imagePath = 'https://qu.ax/CfboF.jpg'
     const imageData = fs.readFileSync(imagePath, { encoding: 'base64' })
     await m.setProfilePic(imageData);
     await m.reply('🌵 Foto de perfil cambiada con éxito.');
