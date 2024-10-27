19065864a6ad94091a8c1d7eb1eb9b821c535d32
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-10-26 21:00:35 -0500

    © ⍴᥆ᥕᥱrᥱძ ᑲᥡ sᥙᥒᥣіgһ𝗍 𝗍ᥱᥲm ❀

--- sunlight.js
+++ sunlight.js
@@ -145,30 +145,40 @@ defaultQueryTimeoutMs: undefined,
 version: [2, 3000, 1015901307],
 }
 
-global.conn = makeWASocket(connectionOptions)
+global.conn = makeWASocket(connectionOptions);
+
 if (!fs.existsSync(`./${sessions}/creds.json`)) {
 if (opcion === '2' || methodCode) {
+
 opcion = '2'
-if (!conn.authState.creds.registered) {
-let addNumber
+if (!conn.authState.creds.registered) {  
+if (MethodMobile) throw new Error('No se puede usar un código de emparejamiento con la API móvil')
+
+let numeroTelefono
 if (!!phoneNumber) {
-addNumber = phoneNumber.replace(/[^0-9]/g, '')
+numeroTelefono = phoneNumber.replace(/[^0-9]/g, '')
+if (!await isValidPhoneNumber(phoneNumber)) {
+console.log(chalk.bgBlack(chalk.bold.greenBright(`🟣  Por favor, Ingrese el número de WhatsApp.\n${chalk.bold.yellowBright("CONSEJO: Copie el número de WhatsApp y péguelo en la consola.")}\n${chalk.bold.yellowBright("Ejemplo: 57321××××××")}\n${chalk.bold.magentaBright('---> ')}`)))
+process.exit(0)
+}} else {
+while (true) {
+numeroTelefono = await question(chalk.bgBlack(chalk.bold.yellowBright('Por favor, escriba su número de WhatsApp.\nEjemplo: 57321××××××\n')))
+numeroTelefono = numeroTelefono.replace(/[^0-9]/g, '')
+
+if (numeroTelefono.match(/^\d+$/) && await isValidPhoneNumber(numeroTelefono)) {
+break 
 } else {
-do {
-phoneNumber = await question(chalk.bgBlack(chalk.bold.greenBright('y el numero?')))
-phoneNumber = phoneNumber.replace(/\D/g,'')
-if (!phoneNumber.startsWith('+')) {
-phoneNumber = `+${phoneNumber}`
-}
-} while (!await isValidPhoneNumber(phoneNumber))
-rl.close()
-addNumber = phoneNumber.replace(/\D/g, '')
+console.log(chalk.bgBlack(chalk.bold.redBright("Por favor, escriba su número de WhatsApp.\nEjemplo: 57321××××××.\n")))
+}}
+rl.close()  
+} 
+
 setTimeout(async () => {
-let codeBot = await conn.requestPairingCode(addNumber)
-codeBot = codeBot?.match(/.{1,4}/g)?.join("-") || codeBot
-console.log(chalk.bold.white(chalk.bgMagenta(`CODE:`), chalk.bold.white(chalk.white(codeBot)))
-}, 2000)
-}}}
+let codigo = await conn.requestPairingCode(numeroTelefono)
+codigo = codigo?.match(/.{1,4}/g)?.join("-") || codigo
+console.log(chalk.black(chalk.bgGreen(`👑 CÓDIGO DE VINCULACIÓN 👑`)), chalk.black(chalk.white(codigo)))
+}, 3000)
+}}
 }
 
 conn.isInit = false;
