20c0eecf97f470dafd552bdf872011162bb5b554
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-10-30 09:56:23 -0500

    © YaemoriBot-MD

--- plugins/Rg•unreg.js
+++ plugins/Rg•unreg.js
@@ -2,9 +2,10 @@ let handler = async (m, { conn, text }) => {
 
 let user = global.db.data.users[m.sender]
 
+setTimeout(async function() {
 user.registered = false
 return conn.reply(m.chat, `🚩 Usted anuló su registro de *YaemoriBot-MD 🌻✨️*\n\n🚩 Puede usar #reg nombre.edad para realizar un nuevo registro`, m, rcanal)
-
+}, 3000 * i)
 
 await conn.sendMessage(global.channelid, { text: '`REGISTRO ANULADO`\n\n👤 User: ' + m.pushName || 'Anónimo', contextInfo: {
 externalAdReply: {
