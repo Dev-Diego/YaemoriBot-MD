c56478cb252cb7532ce687f6de3bc1d59b0aecef
Patito Ofc <theyaemoribot@gmail.com>
2024-10-30 09:45:49 -0500

    © YaemoriBot-MD

--- plugins/Rg•unreg.js
+++ plugins/Rg•unreg.js
@@ -3,7 +3,21 @@ let handler = async (m, { conn, text }) => {
 let user = global.db.data.users[m.sender]
 
 user.registered = false
-return conn.reply(m.chat, `*『✅』 Usted Ya No Está En Mi Base De Datos*`, m, rcanal)
+return conn.reply(m.chat, `🚩 *Usted anuló su registro de *YaemoriBot-MD 🌻✨️*
+
+🚩 *Puede usar #reg nombre.edad para realizar un nuevo registro*`, m, rcanal)
+
+let chtxt = '`REGISTRO ANULADO`\n\n👤 User: ${user}'
+await conn.sendMessage(global.channelid, { text: chtxt, contextInfo: {
+externalAdReply: {
+title: "🔔 Notificación General 🔔 ",
+body: '🐢 Un usuario anuló su registrado',
+thumbnailUrl: fotoperfil,
+sourceUrl: redes,
+mediaType: 1,
+showAdAttribution: false,
+renderLargerThumbnail: false
+}}}, { quoted: null })
 
 }
 handler.help = ['unreg']
