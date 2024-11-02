561e776213ad6425def9b791a4fad394572fe38c
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-11-02 15:05:18 -0500

    © YaemoriBot-MD

--- plugins/Rg•verificar.js
+++ plugins/Rg•verificar.js
@@ -1,88 +1,52 @@
-import { createHash } from 'crypto'  
+import db from '../lib/database.js'
+import { createHash } from 'crypto'
+import fs from 'fs'
 import fetch from 'node-fetch'
-let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i 
+
+let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
 let handler = async function (m, { conn, text, usedPrefix, command }) {
-let codigosIdiomas = ['es', 'en']
-let nombresIdiomas = {
-'es': 'Español',
-'en': 'English'
-}
-
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let pp = await conn.profilePictureUrl(who, 'image').catch(_ => icons)
-
-function pickRandom(list) {
-return list[Math.floor(Math.random() * list.length)]
-} 
-let tag = `${m.sender.split("@")[0]}`
-let aa = tag + '@s.whatsapp.net'
-let user = global.db.data.users[m.sender]
-
-if (/^(verify|verificar|reg(ister)?)$/i.test(command)) {
-if (user.registered === true) return m.reply('Ya está registrado.')
-if (!Reg.test(text)) return m.reply('Y el nonbre')
-let [_, name, splitter, age] = text.match(Reg)  
-if (!name) return m.reply(lenguajeGB.smsVerify2())
-if (!age) return m.reply(lenguajeGB.smsVerify3())
-age = parseInt(age)
-if (age > 50) return m.reply(lenguajeGB.smsVerify4()) 
-if (age < 10) return m.reply(lenguajeGB.smsVerify5())
-if (name.length >= 30) return m.reply(lenguajeGB.smsVerify6())
-user.name = name + '✓ᚲ'.trim()
-user.age = age
-let listaIdiomasTexto = ''
-listaIdiomasTexto += `🚩 Elije tu idioma.\n\n[ 1 ] Español\n[ 2 ] Ingles\n\nEjemplo: #idioma 2`
-await conn.sendMessage(m.chat, { text: genText }, { quoted: m })        
-} 
-
-if (command == 'idioma') {        
-if (!user.name || !user.age) return conn.sendMessage(m.chat, { text: `🌺 Verifique su edad y nombre para poder usar este comando.` }, { quoted: m })   
-var emojiANumero = { "0", "1", "2" }
-text = text.replace(/[\u{0030}-\u{0039}]\u{FE0F}\u{20E3}/gu, function(match) {
-return emojiANumero[match] || match
-})
-let idioma = ''
-function asignarIdioma(text) { 
-if (!text) return conn.sendMessage(m.chat, { text: `🚩 Escriba el numero del idioma\n\n>[ 1 ] Español.\n[ 2 ] Ingles.` }, { quoted: m })          
-if (text < 1 || (text > 5 && text)) {
-conn.reply(m.chat, `*"${text}" no es válido.`, m) 
-}
-switch (text) {
-case "1":
-idioma = 'es'
-break
-case "2":
-idioma = 'en'
-break
-default:
-if (text == 0 || text > 5) return
-return conn.reply(m.chat, `🚩 Recuerda usar el numero del idioma.\n\nEjemplo: #idioma 2`, m)
-}
-asignarIdioma(text)
-user.Language = idioma
-if (!user.Language) return m.reply(`🌺 No se logró configurar el idioma.`)
-if (codigosIdiomas.includes(user.Language)) {
-nombresIdiomas = nombresIdiomas[user.Language]
-} else {
-nombresIdiomas = `🌻 Idioma no detectado.`
-}  
-user.regTime = + new Date
-user.registered = true
-let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6)        
-let caption = `👤 𝗥 𝗘 𝗚 𝗜 𝗦 𝗧 𝗥 𝗢 👤
-•┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄•
-「💭」𝗡𝗼𝗺𝗯𝗿𝗲: ${user.name}
-「✨️」𝗘𝗱𝗮𝗱: ${user.age} años
-•┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄•
-「🎁」𝗥𝗲𝗰𝗼𝗺𝗽𝗲𝗻𝘀𝗮𝘀:
-• 15 Cookies 🍪
-• 5 MiniCoins 🪙
-• 245 Experiencia 💸
-• 12 Tokens 💰
-•┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄•
-⪛✰ 𝐀𝐢 𝐘𝐚𝐞𝐦𝐨𝐫𝐢 - 𝐌𝐃 ✰⪜`.trim()
-await conn.sendFile(m.chat, pp, 'yaemori.jpg', caption, m, false, { mentions: [aa] }) 
-/*await conn.sendMessage(global.channelid, { text: chtxt, contextInfo: {
+  let user = global.db.data.users[m.sender]
+  let name2 = conn.getName(m.sender)
+  if (user.registered === true) return m.reply(`🍭 Ya estás registrado.\n\n*¿Quiere volver a registrarse?*\n\nUse este comando para eliminar su registro.\n*${usedPrefix}unreg*`)
+  if (!Reg.test(text)) return m.reply(`🌹 Formato incorrecto.\n\nUso del comamdo: *${usedPrefix + command} nombre.edad*\nEjemplo : *${usedPrefix + command} ${name2}.666*`)
+  let [_, name, splitter, age] = text.match(Reg)
+  if (!name) return m.reply('🚩 El nombre no puede estar vacío.')
+  if (!age) return m.reply('🚩 La edad no puede estar vacía.')
+  if (name.length >= 100) return m.reply('🚩 El nombre es demasiado largo.' )
+  age = parseInt(age)
+  if (age > 100) return m.reply('👴🏻 Wow el abuelo quiere jugar al bot.')
+  if (age < 5) return m.reply('🚼  hay un abuelo bebé jsjsj. ')
+  user.name = name + '✓'.trim()
+  user.age = age
+  user.regTime = + new Date
+  user.registered = true
+  global.db.data.users[m.sender].money += 600
+  global.db.data.users[m.sender].cookies += 15
+  global.db.data.users[m.sender].exp += 245
+  global.db.data.users[m.sender].joincount += 5
+  let sn = createHash('md5').update(m.sender).digest('hex')
+let regbot = `👤 𝗥 𝗘 𝗚 𝗜 𝗦 𝗧 𝗥 𝗢 👤\n`
+regbot += `•┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄•\n`
+regbot += `「💭」𝗡𝗼𝗺𝗯𝗿𝗲: ${name}\n`
+regbot += `「✨️」𝗘𝗱𝗮𝗱: ${age} años\n`
+regbot += `•┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄•\n`
+regbot += `「🎁」𝗥𝗲𝗰𝗼𝗺𝗽𝗲𝗻𝘀𝗮𝘀:\n`
+regbot += `• 15 Cookies 🍪\n`
+regbot += `• 5 MiniCoins 🪙\n`
+regbot += `• 245 Experiencia 💸\n`
+regbot += `• 12 Tokens 💰\n`
+regbot += `•┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄•\n`
+regbot += `${packname}`
+await m.react('📩')
+await conn.sendMini(m.chat, '⊱『✅𝆺𝅥 𝗥𝗘𝗚𝗜𝗦𝗧𝗥𝗔𝗗𝗢(𝗔) 𝆹𝅥✅』⊰', textbot, regbot, imagen1, imagen1, channel, m)
+let chtxt = `
+👤 *Usuario* » ${m.pushName || 'Anónimo'}
+🌎 *Pais* » ${global.userNationality}
+🗃 *Verificación* » ${user.name}
+🌺 *Edad* » ${user.age}
+📆 *Fecha* » ${moment.tz('America/Bogota').format('DD/MM/YY')}
+`.trim()
+await conn.sendMessage(global.channelid, { text: chtxt, contextInfo: {
 externalAdReply: {
 title: "【 🔔 𝗡𝗢𝗧𝗜𝗙𝗜𝗖𝗔𝗖𝗜𝗢́𝗡 🔔 】",
 body: '🥳 ¡Un usuario nuevo en mi base de datos!',
@@ -91,8 +55,10 @@ sourceUrl: redes,
 mediaType: 1,
 showAdAttribution: false,
 renderLargerThumbnail: false
-}}}, { quoted: null })*/
+}}}, { quoted: null })
 }
-}
-handler.command = ['verify', 'verificar', 'reg', 'register', 'idioma']
+handler.help = ['reg']
+handler.tags = ['rg']
+handler.command = ['verify', 'verificar', 'reg', 'register', 'registrar'] 
+
 export default handler
