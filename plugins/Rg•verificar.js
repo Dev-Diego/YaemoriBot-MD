0e333d67b9bb609d3fdd378223cad37839b6a550
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-11-01 20:00:33 -0500

    © YaemoriBot-MD

--- plugins/Rg•verificar.js
+++ plugins/Rg•verificar.js
@@ -16,7 +16,7 @@ let handler = async function (m, { conn, text, usedPrefix, command }) {
   age = parseInt(age)
   if (age > 100) return m.reply('👴🏻 Wow el abuelo quiere jugar al bot.')
   if (age < 5) return m.reply('🚼  hay un abuelo bebé jsjsj. ')
-  user.name = name.trim()
+  user.name = name + '✓'.trim()
   user.age = age
   user.regTime = + new Date
   user.registered = true
