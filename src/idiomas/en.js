aa64be1c023be8ca2c0cc4176812063c3256389e
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-11-02 15:32:17 -0500

    © YaemoriBot-MD

--- src/idiomas/en.js
+++
@@ -1,6 +0,0 @@
-const lenguaje = () => { return 'en' } //Ingles
-
-const gemini = () => { return `『🪐』It seems that the coronavirus is not a problem for us.\n\n• 𝙋𝙤𝙧 𝙚𝙟𝙚𝙢𝙥𝙡𝙤:\n` }
-const geminierror = () => { return `『📣』It is always a good time for us to be patient, we always take care of you.` } 
-
-export default { lenguaje, gemini, geminierror }
--- src/idiomas/total-idiomas.js
+++
@@ -1,5 +0,0 @@
-import espanol from './es.js'; //Español 
-import ingles from './en.js'; //Ingles
-
-export const es = espanol
-export const en = ingles
