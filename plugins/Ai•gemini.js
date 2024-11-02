95ae2825905d31de7923e6a4067e6086e5d59f5d
Patito Ofc <theyaemoribot@gmail.com>
2024-11-02 13:17:03 -0500

    © YaemoriBot-MD

--- plugins/Ai•gemini.js
+++ plugins/Ai•gemini.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 
 var handler = async (m, { text,  usedPrefix, command }) => {
-const lang = global.db.data.users[anu.participants]?.lenguaje || 'es'
+const lang = global.db.data.users[m.sender]?.lenguaje || 'es'
 const _translate = JSON.parse(fs.readFileSync(`./src/idiomas/${lang}.json`))
 const tradutor = _translate.plugins.ai_gemini
 
