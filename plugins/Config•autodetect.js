e4cee6ad03eea7da2cccb8c0a962fde7251ec905
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-11-02 17:42:18 -0500

    © YaemoriBot-MD

--- plugins/Config•autodetect.js
+++ plugins/Config•autodetect.js
@@ -14,7 +14,7 @@ edit = `*${usuario}*\n🌺 Ha permitido que ${m.messageStubParameters[0] == 'on'
 newlink = `🌸 El enlace de  *${groupMetadata.subject}* ha sido restablecido por:\n*» ${usuario}*`
 status = `*${groupMetadata.subject}* Ha sido ${m.messageStubParameters[0] == 'on' ? '*cerrado 🔒*' : '*abierto 🔓*'} Por *${usuario}*\n\n💬 Ahora ${m.messageStubParameters[0] == 'on' ? '*solo admins*' : '*todos*'} pueden enviar mensaje`
 admingp = `*@${m.messageStubParameters[0].split`@`[0]}* Ahora es admin del grupo 🥳\n\n💫 Acción hecha por:\n*» ${usuario}*`
-noadmingp =  `*@${m.messageStubParameters[0].split`@`[0]}* Deja de ser admin del grupo 😿\n\n💫 *Acción hecha por:\n*» ${usuario}*`
+noadmingp =  `*@${m.messageStubParameters[0].split`@`[0]}* Deja de ser admin del grupo 😿\n\n💫 Acción hecha por:\n*» ${usuario}*`
 
 if (chat.detect && m.messageStubType == 21) {
 await conn.sendMessage(m.chat, { text: nombre, mentions: [m.sender] }, { quoted: fkontak })   
