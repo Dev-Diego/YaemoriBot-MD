abc80a4134d33ae182a4eeb0de5bbbb57b5d5af3
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-11-02 15:29:56 -0500

    © YaemoriBot-MD

--- plugins/Ai•gemini.js
+++ plugins/Ai•gemini.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 
 var handler = async (m, { text,  usedPrefix, command }) => {
 
-if (!text) return conn.reply(m.chat, `${languaje['gemini']()} ${usedPrefix + command} Hola`, m, rcanal)
+if (!text) return conn.reply(m.chat, `『🪐』𝙄𝙣𝙜𝙧𝙚𝙨𝙚 𝙪𝙣 𝙩𝙚𝙭𝙩𝙤 𝙥𝙖𝙧𝙖 𝙪𝙨𝙖𝙧 𝙚𝙨𝙩𝙚 𝙘𝙤𝙢𝙖𝙣𝙙𝙤.\n\n• 𝙋𝙤𝙧 𝙚𝙟𝙚𝙢𝙥𝙡𝙤: ${usedPrefix + command} Hola`, m, rcanal)
 try {
 await m.react(rwait)
 conn.sendPresenceUpdate('composing', m.chat)
@@ -11,7 +11,7 @@ var res = await apii.json()
 await m.reply(res.result)
 } catch {
 await m.react(error)
-await conn.reply(m.chat, `${lenguaje['geminierror']()}`, m, rcanal)
+await conn.reply(m.chat, `『📣』𝙊𝙘𝙪𝙧𝙧𝙞𝙤 𝙪𝙣 𝙚𝙧𝙧𝙤𝙧 𝙚𝙣 𝙚𝙡 𝙘𝙤𝙢𝙖𝙣𝙙𝙤, 𝙧𝙚𝙥𝙤𝙧𝙩𝙖𝙡𝙤 𝙖𝙡 𝙘𝙧𝙚𝙖𝙙𝙤𝙧 𝙙𝙚𝙡 𝙗𝙤𝙩.`, m, rcanal)
 }}
 handler.command = ['gemini']
 handler.help = ['gemini']
