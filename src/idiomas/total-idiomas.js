ca82c87f8a4212810ea8439a9394ec6d0437597b
Patito Ofc <theyaemoribot@gmail.com>
2024-11-02 13:56:59 -0500

    © YaemoriBot-MD

--- src/idiomas/total-idiomas.js
+++ src/idiomas/total-idiomas.js
@@ -1,5 +1,5 @@
-import espanol from './es.js'; //Español 
-import ingles from './en.js'; //Ingles
+import es from './es.js'; //Español 
+import en from './en.js'; //Ingles
 
-export const es = espanol
-export const en = ingles
+export const es = es
+export const en = en
