5d5bb7b62619b78678a3ab9cc43aa96f9d7cbfbd
Patito Ofc <theyaemoribot@gmail.com>
2024-11-02 12:24:08 -0500

    © YaemoriBot-MD

--- plugins/Ai•gemini.js
+++ plugins/Ai•gemini.js
@@ -1,6 +1,13 @@
 import fetch from 'node-fetch'
+//Lenguaje
+let lang = global.db.data.users[m.sender]?.lenguaje || 'es'
+const L = JSON.parse(fs.readFileSync(`./lib/idiomas/${lang}.json`))
+const { 
+ai: { gemini }
+} = L
+
 var handler = async (m, { text,  usedPrefix, command }) => {
-if (!text) return conn.reply(m.chat, `『🪐』𝙄𝙣𝙜𝙧𝙚𝙨𝙚 𝙪𝙣 𝙩𝙚𝙭𝙩𝙤 𝙥𝙖𝙧𝙖 𝙪𝙨𝙖𝙧 𝙚𝙨𝙩𝙚 𝙘𝙤𝙢𝙖𝙣𝙙𝙤.\n\n• 𝙋𝙤𝙧 𝙚𝙟𝙚𝙢𝙥𝙡𝙤:\n${usedPrefix + command} Hola`, m, rcanal)
+if (!text) return conn.reply(m.chat, `${gemini.text1}`, m, rcanal)
 try {
 await m.react(rwait)
 conn.sendPresenceUpdate('composing', m.chat)
@@ -9,7 +16,7 @@ var res = await apii.json()
 await m.reply(res.result)
 } catch {
 await m.react(error)
-await conn.reply(m.chat, `『📣』𝙊𝙘𝙪𝙧𝙧𝙞𝙤 𝙪𝙣 𝙚𝙧𝙧𝙤𝙧 𝙚𝙣 𝙚𝙡 𝙘𝙤𝙢𝙖𝙣𝙙𝙤, 𝙧𝙚𝙥𝙤𝙧𝙩𝙖𝙡𝙤 𝙖𝙡 𝙘𝙧𝙚𝙖𝙙𝙤𝙧 𝙙𝙚𝙡 𝙗𝙤𝙩.`, m, rcanal)
+await conn.reply(m.chat, `${gemini.texterror}`, m, rcanal)
 }}
 handler.command = ['gemini']
 handler.help = ['gemini']
