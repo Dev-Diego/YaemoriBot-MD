5a5f2a0725845238c1b29cbfff70d324e4ed801a
Patito Ofc <theyaemoribot@gmail.com>
2024-11-03 10:45:54 -0500

    © YaemoriBot-MD

--- plugins/Descargas•play.js
+++ plugins/Descargas•play.js
@@ -2,7 +2,6 @@ import yts from 'yt-search'
 const handler = async (m, { conn, text, usedPrefix, command }) => {
 if (!text) return conn.reply(m.chat, `🚩 *Ingrese el nombre de un video de YouTube*\n\nEjemplo, !${command} Distancia - Kimberly Contreraxx`,  m, rcanal);
 
-try {
 await m.react(rwait)
 const randomReduction = Math.floor(Math.random() * 5) + 1;
 let search = await yts(text);
@@ -27,10 +26,10 @@ let audio = res.data.mp3;
 await conn.sendMessage(m.chat, { [type]: { url: isVideo ? video : audio }, gifPlayback: false, mimetype: isVideo ? "video/mp4" : "audio/mpeg" }, { quoted: fkontak });
 await m.react(done)
 
-} catch {
+/*} catch {
 await conn.reply(m.chat, `✘ *Ocurrío un error*`, m, rcanal)
 await m.react(error)
-}}
+}*/}
 
 handler.command = ['play', 'play2'];
 export default handler;
