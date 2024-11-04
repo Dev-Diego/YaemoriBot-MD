0823a3c69098c176d303d24a56d454223ca2a076
Patito Ofc <theyaemoribot@gmail.com>
2024-11-04 14:41:04 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Herramientas•deobfuscate.js
+++ plugins/Herramientas•deobfuscate.js
@@ -1,30 +1,25 @@
-let handler = async (m, { conn, args, text, usedPrefix, command }) => {
+let handler = async (m, { conn, args, text }) => {
 
-if (!text) return conn.reply(m.chat, '🔓 Que codigo deseas deobfuscar?', m, rcanal)
+if (!text) return conn.reply(m.chat, '🚩 Por favor, ingresa el texto que deseas desencriptar.', m);
 
-try {
-await m.react(rwait)
-const deobfuscateOfc = args.join(" ")
-const offset = 5
-const Texto = (input) => {
-const used = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-const Ofcdeobfuscate = input.split('').map(char => {
-const index = used.indexOf(char);
-if (index === -1) return char 
-return used[(index - offset + used.length) % used.length]}).join('')
-return Ofcdeobfuscate
-}
-const deobfuscate = Texto(deobfuscateOfc)
+const encryptedText = args.join(" ");
+const offset = 5; 
+const decryptText = (input) => {
+const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+const decrypted = input.split('').map(char => {
+const index = chars.indexOf(char);
+if (index === -1) return char; 
+return chars[(index - offset + chars.length) % chars.length];
+}).join('');
+return decrypted;
+};
 
-} catch (e) {
-await m.reply('❌️ Ocurrió un error.\n\n' + e)
-await m.react(error)
+const decryptedMessage = decryptText(encryptedText);
+conn.reply(m.chat, decryptedMessage, m, fake);
+};
 
-await conn.reply(m.chat, deobfuscate, m, rcanal)
-await m.react(done)}}
+handler.help = ['deobfuscate <texto>'];
+handler.tags = ['tools']:
+handler.command = ['deobfuscate', 'desencriptar'];
 
-handler.help = ['deobfuscate <texto>']
-handler.tags = ['tools']
-handler.command = ['deobfuscate', 'desencriptar']
-
-export default handler
+export default handler;
