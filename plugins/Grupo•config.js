7eb2bd9db663a75ce9c32e3e5d946ae916629dca
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-11-05 21:54:40 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Grupo•config.js
+++ plugins/Grupo•config.js
@@ -1,15 +1,28 @@
-var handler = async (m, {conn, args, usedPrefix, command}) => {
-const isClose = { 'open': 'not_announcement', 'close': 'announcement', 'abierto': 'not_announcement', 'cerrado': 'announcement', 'abrir': 'not_announcement', 'cerrar': 'announcement', 'desbloquear': 'unlocked', 'bloquear': 'locked' }[(args[0] || '')]
-if (isClose === undefined) { return conn.reply(m.chat, `*Elija una opción para configurar el grupo*\n\nEjemplo:\n*○ !${command} abrir*\n*○ !${command} cerrar*\n*○ !${command} bloquear*\n*○ !${command} desbloquear*`, m, rcanal, )}
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './src/grupos.jpg'  
+let isClose = { // Switch Case Like :v
+'open': 'not_announcement',
+'close': 'announcement',
+'abierto': 'not_announcement',
+'cerrado': 'announcement',
+'abrir': 'not_announcement',
+'cerrar': 'announcement',
+}[(args[0] || '')]
+if (isClose === undefined)
+return conn.reply(m.chat, `*Elija una opción para configurar el grupo*\n\nEjemplo:\n*○ !${command} abrir*\n*○ !${command} cerrar*\n*○ !${command} bloquear*\n*○ !${command} desbloquear*`, m, rcanal)
 await conn.groupSettingUpdate(m.chat, isClose)
-{ 
-conn.reply(m.chat, '✅ *Configurado correctamente*', m, rcanal, )
-await m.react(done)
+
+if (isClose === 'not_announcement'){
+m.reply(`🔓 *YA PUEDEN ESCRIBIR EN ESTE GRUPO.*`)
+}
+
+if (isClose === 'announcement'){
+m.reply(`🔐 *SOLOS LOS ADMINS PUEDEN ESCRIBIR EN ESTE GRUPO.*`)
 }}
-handler.help = ['group abrir / cerrar']
-handler.tags = ['grupo']
+handler.help = ['group open / close', 'grupo abrir / cerrar']
+handler.tags = ['group']
 handler.command = ['group', 'grupo']
 handler.admin = true
 handler.botAdmin = true
-
+handler.exp = 200
 export default handler
