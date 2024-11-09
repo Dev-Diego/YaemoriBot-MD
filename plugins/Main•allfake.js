78984f81328d73ad6f8025ac6be7bc105825cb6c
Patito Ofc <theyaemoribot@gmail.com>
2024-11-08 22:02:12 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Main•allfake.js
+++ plugins/Main•allfake.js
@@ -123,9 +123,9 @@ global.fake = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo
 }}, { quoted: m }
 
 global.icono = [ 
-'https://qu.ax/uuYfC.jpg',
-'https://qu.ax/fprhC.jpg',
-'https://qu.ax/YHLEj.jpg'
+'https://qu.ax/jqbRA.jpg',
+'https://qu.ax/scObN.jpg',
+'https://qu.ax/nqXmE.jpg'
 ].getRandom()
 
 global.rcanal = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, serverMessageId: 100, newsletterName: channelRD.name, }, externalAdReply: { showAdAttribution: true, title: namebot, body: dev, mediaUrl: null, description: null, previewType: "PHOTO", thumbnailUrl: icono, sourceUrl: redes, mediaType: 1, renderLargerThumbnail: false }, }, }}
