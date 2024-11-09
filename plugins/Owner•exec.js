0979144378e3f2accb489d8c1688396825ee7e3e
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-11-09 13:15:59 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Owner•exec.js
+++ plugins/Owner•exec.js
@@ -38,7 +38,7 @@ let handler = async (m, _2) => {
   }
 }
 handler.help = ['> ', '=> ']
-handler.tags = ['advanced']
+handler.tags = ['owner']
 handler.customPrefix = /^=?> /
 handler.command = /(?:)/i
 handler.rowner = true
