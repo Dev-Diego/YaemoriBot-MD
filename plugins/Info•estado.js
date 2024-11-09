4d94512d74ff0e60b32468172191e24f7beaa090
Patito Ofc <theyaemoribot@gmail.com>
2024-11-08 22:05:11 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Info•estado.js
+++ plugins/Info•estado.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, usedPrefix, isRowner}) => {
 let _muptime
 let totalreg = Object.keys(global.db.data.users).length
 let totalchats = Object.keys(global.db.data.chats).length
-let pp = icons
+let pp = 'https://qu.ax/JceST.jpg'
 if (process.send) {
 process.send('uptime')
 _muptime = await new Promise(resolve => {
