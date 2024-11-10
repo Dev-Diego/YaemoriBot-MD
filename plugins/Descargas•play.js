6a116977167248fc83d6e2b620b3933c068d1f45
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-11-10 18:29:20 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Descargas•play.js
+++ plugins/Descargas•play.js
@@ -1,77 +1,92 @@
-/*
+// @Kenisawa
 
-- PLUGIN PLAY YOUTUBE 2
-- By Kenisawa
+import axios from 'axios';
+import yts from 'yt-search';
 
-*/
-import yts from "yt-search"
-import _ from "lodash"
+let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-let handler = async (m, { conn, command, usedPrefix, args }) => {
-  const text = _.get(args, "length") ? args.join(" ") : _.get(m, "quoted.text") || _.get(m, "quoted.caption") || _.get(m, "quoted.description") || ""
-  if (typeof text !== 'string' || !text.trim()) return m.reply(`✦ Ingresa una consulta\n*Ejemplo:* .${command} Joji Ew`)
+  if (!text) return conn.reply(m.chat, `🚩 *Ingrese el nombre de un video de YouTube*\n\nEjemplo, !${command} Distancia - Kimberly Contreraxx`,  m, rcanal, )
+  
+    let results = await yts(text);
+    let tes = results.all[0]
+    
+const baseUrl = 'https://cuka.rfivecode.com';
+const cukaDownloader = {
+  youtube: async (url, exct) => {
+    const format = [ 'mp3', 'mp4' ];
+    try {
+      await m.react(rwait)
+      conn.reply(m.chat, global.wait, m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
+      title: packname,
+      body: dev,
+      previewType: 0, thumbnail: icons,
+      sourceUrl: channel }}})
+      const response = await fetch(`${baseUrl}/download`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+          body: JSON.stringify({ url, format: exct })
+      });
 
-  await m.reply('✦ Espere un momento...')
+      const data = await response.json();
+      return data;
+      console.log('Data:' + data);
+    } catch (error) {
+      return { success: false, message: error.message };
+      console.error('Error:', error);
+    }
+  },
+  tiktok: async (url) => {
+    try {
+      await m.react(rwait)
+      const response = await fetch(`${baseUrl}/tiktok/download`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+          body: JSON.stringify({ url })
+      });
 
-  const vid = await ytsearch(text)
-  if (!vid?.url) return m.reply("Audio no encontrado, intenta usando otra consulta.")
+      const data = await response.json();
+      return data;
+      console.log('Data:' + data);
+    } catch (error) {
+      return { success: false, message: error.message };
+      console.error('Error:', error);
+    }
+  },
+  spotify: async (url) => {
+    try {
+      await m.react(rwait)
+      const response = await fetch(`${baseUrl}/spotify/download`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+          body: JSON.stringify({ url })
+      });
 
-  const { title = "No encontrado", thumbnail, timestamp = "No encontrado", views = "No encontrado", ago = "No encontrado", url } = vid
-
-  const captvid = ` *✦Título:* ${title}\n *✧Duración:* ${timestamp}\n *✧Publicado:* ${ago}\n *✦Link:* ${url}`
-
-  const ytthumb = (await conn.getFile(thumbnail))?.data
-
-  const infoReply = {
-    contextInfo: {
-      externalAdReply: {
-        body: `✧ En unos momentos se entrega su audio`,
-        mediaType: 1,
-        mediaUrl: url,
-        previewType: 0,
-        renderLargerThumbnail: true,
-        sourceUrl: url,
-        thumbnail: ytthumb,
-        title: `Y O U T U B E - A U D I O`
-      }
+      const data = await response.json();
+      return data;
+      console.log('Data:' + data);
+    } catch (error) {
+      return { success: false, message: error.message };
+      console.error('Error:', error);
     }
   }
-
-  await conn.reply(m.chat, captvid, m, infoReply)
-  infoReply.contextInfo.externalAdReply.body = `Audio descargado con éxito`
-
-  const res = await fetch(`https://api.zenkey.my.id/api/download/ytmp3?apikey=zenkey&url=${url}`)
-  const audioData = await res.json()
-  
-  if (audioData.status && audioData.result?.downloadUrl) {
-
-await conn.sendMessage(m.chat, { video: { url: audioData.result.downloadUrl }, fileName: `${n}.mp4`, mimetype: 'video/mp4', caption: `🚩`, thumbnail: audioData.result.downloadUrl }, { quoted: m })
-
-/*    await conn.sendMessage(m.chat, {
-      audio: { url: audioData.result.downloadUrl },
-      caption: captvid,
-      mimetype: "audio/mpeg",
-      contextInfo: infoReply.contextInfo
-    }, { quoted: m })*/
-  } else {
-    await m.reply("Error al descargar el audio.")
-  }
 }
 
-handler.help = ["play2 <consulta>"]
-handler.tags = ["downloader"]
-handler.command = ['play2']
-handler.limit = true
-export default handler
-
-async function ytsearch(query, maxResults = 5, similarityThreshold = .5) {
-  const res = await yts(query)
-  const videos = _.filter(res.videos.slice(0, maxResults), video => {
-    const titleWords = _.words(_.toLower(video.title))
-    const queryWords = _.words(_.toLower(query))
-    const matchedWords = _.intersection(titleWords, queryWords)
-    const similarity = _.size(matchedWords) / _.size(titleWords)
-    return similarity >= similarityThreshold || _.size(matchedWords) >= _.size(queryWords) - 1
-  })
-  return _.isEmpty(videos) ? {} : _.first(videos)
+let dataos = await cukaDownloader.youtube(tes.url, "mp3")
+console.log(dataos)
+let { title, thumbnail, quality, downloadUrl } = dataos
+  await conn.sendMessage(m.chat, { audio: { url: downloadUrl }, fileName: title + '.mp3', mimetype: 'audio/mp4' }, { quoted: fkontak });
+  await m.react(done);
 }
+handler.help = ['play'];
+handler.tags = ['descargas'];
+handler.command = ['play', 'mp3'];
+handler.register = true
+
+export default handler;
