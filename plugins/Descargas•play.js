1a3b3bf61a5d81ece2975099603405aff3f72802
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-11-10 17:38:37 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Descargas•play.js
+++ plugins/Descargas•play.js
@@ -1,106 +1,190 @@
-// @Kenisawa
+import axios from 'axios'
+import yts from 'yt-search'
+import _ from 'lodash' 
 
-import axios from 'axios';
-import yts from 'yt-search';
-import _ from 'lodash';
+let handler = async (m, { conn, args, usedPrefix, text, command }) => {
+let lister = ["mp3", "yta", "audio", "ytv", "video", "vídeo", "mp4", "mp3doc", "ytadoc", "audiodoc", "mp4doc", "ytvdoc", "videodoc", "vídeodoc"]
 
-let handler = async (m, { conn, text, usedPrefix, command }) => {
+let [feature, inputs, inputs_, inputs__, inputs___] = text.split(" ")
+if (!lister.includes(feature)) return conn.reply(m.chat, `🚩 Ingresa el formato en que deseas descargar más el titulo de un video o musica de YouTube.\n\nEjemplo : ${usedPrefix + command} *mp3* Adan y Eva - Paulo Londra\n\nFormatos disponibles :\n${usedPrefix + command} *mp3*\n${usedPrefix + command} *mp3doc*\n${usedPrefix + command} *mp4*\n${usedPrefix + command} *mp4doc*`, m, rcanal)
+if (lister.includes(feature)) {
+if (feature == "mp3" || feature == "yta" || feature == "audio") {
+if (!inputs) return conn.reply(m.chat, `🚩 *Ingrese el nombre de un video de YouTube*\n\nEjemplo, !${command} Distancia - Kimberly Contreraxx`, m, rcanal)
+await m.react(rwait)
+let res = await yts(text)
+let vid = res.videos[0]
+let q = '128kbps'
 
-   if (!text) return star.reply(m.chat, `🚩 *Ingrese el nombre de un video de YouTube*\n\nEjemplo, ${usedPrefix + command} Distancia - Kimberly Contreraxx`, m, rcanal)
-  
-    let results = await yts(text);
-    let tes = results.all[0]
-    
-const baseUrl = 'https://cuka.rfivecode.com';
-const cukaDownloader = {
-  youtube: async (url, exct) => {
-    const format = [ 'mp3', 'mp4' ];
-    try {
-      const response = await fetch(`${baseUrl}/download`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-          body: JSON.stringify({ url, format: exct })
-      });
+try {
+let yt = await fg.yta(vid.url, q)
+let { title, dl_url, size } = yt
+let limit = 100
 
-      const data = await response.json();
-      return data;
-      console.log('Data:' + data);
-    } catch (error) {
-      return { success: false, message: error.message };
-      console.error('Error:', error);
+if (size.split('MB')[0] >= limit) return conn.reply(m.chat,`El archivo pesa mas de ${limit} MB, se canceló la Descarga.`, m, rcanal).then(_ => m.react(error))
+
+await conn.sendMessage(m.chat, { audio: { url: dl_url }, fileName: title + '.mp3', mimetype: 'audio/mp4' }, { quoted: m })
+await m.react(done)
+} catch {
+try {
+let yt = await fg.ytmp3(vid.url, q)
+let { title, dl_url, size } = yt
+let limit = 100
+
+if (size.split('MB')[0] >= limit) return conn.reply(m.chat,`El archivo pesa mas de ${limit} MB, se canceló la Descarga.`, m, rcanal).then(_ => m.react(error))
+
+await conn.sendMessage(m.chat, { audio: { url: dl_url }, fileName: title + '.mp3', mimetype: 'audio/mp4' }, { quoted: m })
+await m.react(done)
+} catch {
+await m.react(error)
+await conn.reply(m.chat, '✘ *Ocurrío un error*', m, fake)
+}}}
+
+if (feature == "mp4" || feature == "ytv" || feature == "video" || feature == "video") {
+if (!inputs) return conn.reply(m.chat, `🚩 *Ingrese el nombre de un video de YouTube*\n\nEjemplo, !${command} Distancia - Kimberly Contreraxx`, m, rcanal)
+await m.react(rwait)
+let res = await yts(text)
+let vid = res.videos[0]
+let q = '360p'
+try {
+let yt = await fg.ytv(vid.url, q)
+let { title, dl_url, size } = yt
+let limit = 100
+
+if (size.split('MB')[0] >= limit) return conn.reply(m.chat,`El archivo pesa mas de ${limit} MB, se canceló la Descarga.`, m, rcanal).then(_ => m.react(error))
+
+await conn.sendFile(m.chat, dl_url, 'yt.jpg', `🚀 Aquí está tu video de youtube.\n${packname}`, m, null, rcanal)
+await m.react(done)
+} catch {
+try {
+let yt = await fg.ytmp4(vid.url, q)
+let { title, dl_url, size } = yt
+let limit = 100
+
+if (size.split('MB')[0] >= limit) return conn.reply(m.chat,`El archivo pesa mas de ${limit} MB, se canceló la Descarga.`, m, rcanal).then(_ => m.react(error))
+
+await conn.sendFile(m.chat, dl_url, 'yt.jpg', `🚀 Aquí está tu video de youtube.\n${packname}`, m, null, rcanal)
+await m.react(done)
+} catch {
+await m.react(error)
+await conn.reply(m.chat, '✘ *Ocurrío un error*', m, fake)
+}}}
+
+if (feature == "mp3doc" || feature == "ytadoc" || feature == "audiodoc") {
+if (!inputs) return conn.reply(m.chat, `🚩 *Ingrese el nombre de un video de YouTube*\n\nEjemplo, !${command} Distancia - Kimberly Contreraxx`, m, rcanal)
+await m.react(rwait)
+let res = await yts(text)
+let vid = res.videos[0]
+let q = '128kbps'
+try {
+let yt = await fg.yta(vid.url, q)
+let { title, dl_url, size } = yt
+let limit = 100
+
+if (size.split('MB')[0] >= limit) return conn.reply(m.chat,`El archivo pesa mas de ${limit} MB, se canceló la Descarga.`, m, rcanal).then(_ => m.react(error))
+
+await conn.sendMessage(m.chat, { document: { url: dl_url }, caption: '', mimetype: 'audio/mpeg', fileName: `${vid.title}.mp3`}, { quoted: m })
+await m.react(done)
+} catch {
+try {
+let yt = await fg.ytmp3(vid.url, q)
+let { title, dl_url, size } = yt
+let limit = 100
+
+if (size.split('MB')[0] >= limit) return conn.reply(m.chat,`El archivo pesa mas de ${limit} MB, se canceló la Descarga.`, m, rcanal).then(_ => m.react(error))
+
+await conn.sendMessage(m.chat, { document: { url: dl_url }, caption: '', mimetype: 'audio/mpeg', fileName: `${vid.title}.mp3`}, { quoted: m })
+} catch {
+await m.react(error)
+await conn.reply(m.chat, '✘ *Ocurrío un error*', m, fake)
+}}}
+
+if (feature == "mp4doc" || feature == "ytvdoc" || feature == "videodoc" || feature == "videodoc") {
+if (!inputs) return conn.reply(m.chat, `🚩 *Ingrese el nombre de un video de YouTube*\n\nEjemplo, !${command} Distancia - Kimberly Contreraxx`, m, rcanal)
+
+await m.react(rwait)
+let res = await yts(text)
+let vid = res.videos[0]
+let q = '360p'
+try {
+let yt = await fg.ytv(vid.url, q)
+let { title, dl_url, size } = yt
+let limit = 300
+
+if (size.split('MB')[0] >= limit) return conn.reply(m.chat,`El archivo pesa mas de ${limit} MB, se canceló la Descarga.`, m, rcanal).then(_ => m.react('✖️'))
+
+await conn.sendMessage(m.chat, { document: { url: dl_url }, caption: '', mimetype: 'video/mp4', fileName: `${vid.title}` + `.mp4`}, {quoted: m })
+await m.react(done)
+} catch {
+try {
+let yt = await fg.ytmp4(vid.url, q)
+let { title, dl_url, size } = yt
+let limit = 300
+
+if (size.split('MB')[0] >= limit) return conn.reply(m.chat,`El archivo pesa mas de ${limit} MB, se canceló la Descarga.`, m, rcanal).then(_ => m.react(error))
+
+await conn.sendMessage(m.chat, { document: { url: dl_url }, caption: '', mimetype: 'video/mp4', fileName: `${vid.title}` + `.mp4`}, {quoted: m })
+await m.react(done)
+} catch {
+await m.react(error)
+await conn.reply(m.chat, '✘ *Ocurrío un error*', m, fake)
+}}}}}
+handler.command = ['play1', 'play12']
+handler.register = true 
+export default handler
+
+function eYear(txt) {
+    if (!txt) {
+        return '×'
     }
-  },
-  tiktok: async (url) => {
-    try {
-      const response = await fetch(`${baseUrl}/tiktok/download`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-          body: JSON.stringify({ url })
-      });
-
-      const data = await response.json();
-      return data;
-      console.log('Data:' + data);
-    } catch (error) {
-      return { success: false, message: error.message };
-      console.error('Error:', error);
+    if (txt.includes('month ago')) {
+        var T = txt.replace("month ago", "").trim()
+        var L = 'hace '  + T + ' mes'
+        return L
     }
-  },
-  spotify: async (url) => {
-    try {
-      await m.react(rwait)
-      conn.reply(m.chat, '🚀 Enviando el audio....', m, {
-      contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
-      title: packname,
-      body: dev,
-      previewType: 0, thumbnail: icons,
-     sourceUrl: channel }}})
-      const response = await fetch(`${baseUrl}/spotify/download`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-          body: JSON.stringify({ url })
-      });
-
-      const data = await response.json();
-      return data;
-      console.log('Data:' + data);
-    } catch (error) {
-      return { success: false, message: error.message };
-      console.error('Error:', error);
+    if (txt.includes('months ago')) {
+        var T = txt.replace("months ago", "").trim()
+        var L = 'hace ' + T + ' meses'
+        return L
     }
-  }
+    if (txt.includes('year ago')) {
+        var T = txt.replace("year ago", "").trim()
+        var L = 'hace ' + T + ' año'
+        return L
+    }
+    if (txt.includes('years ago')) {
+        var T = txt.replace("years ago", "").trim()
+        var L = 'hace ' + T + ' años'
+        return L
+    }
+    if (txt.includes('hour ago')) {
+        var T = txt.replace("hour ago", "").trim()
+        var L = 'hace ' + T + ' hora'
+        return L
+    }
+    if (txt.includes('hours ago')) {
+        var T = txt.replace("hours ago", "").trim()
+        var L = 'hace ' + T + ' horas'
+        return L
+    }
+    if (txt.includes('minute ago')) {
+        var T = txt.replace("minute ago", "").trim()
+        var L = 'hace ' + T + ' minuto'
+        return L
+    }
+    if (txt.includes('minutes ago')) {
+        var T = txt.replace("minutes ago", "").trim()
+        var L = 'hace ' + T + ' minutos'
+        return L
+    }
+    if (txt.includes('day ago')) {
+        var T = txt.replace("day ago", "").trim()
+        var L = 'hace ' + T + ' dia'
+        return L
+    }
+    if (txt.includes('days ago')) {
+        var T = txt.replace("days ago", "").trim()
+        var L = 'hace ' + T + ' dias'
+        return L
+    }
+    return txt
 }
-
-let dataos = await cukaDownloader.youtube(tes.url, "mp3")
-console.log(dataos)
-let { title, thumbnail, quality, downloadUrl } = dataos
-
-      const doc = {
-      audio: { url: downloadUrl },
-      mimetype: 'audio/mp4',
-      fileName: `${title}.mp3`,
-      contextInfo: {
-        externalAdReply: {
-          showAdAttribution: true,
-          mediaType: 2,
-          mediaUrl: tes.url,
-          title: title,
-          sourceUrl: tes.url,
-          thumbnail: await (await conn.getFile(thumbnail)).data
-        }
-      }
-    };
-    await conn.sendMessage(m.chat, doc, { quoted: m });
-}
-handler.help = ['play', 'play2'];
-handler.tags = ['descargas'];
-handler.command = ['play', 'play2'];
-handler.register = true;
-
-export default handler;
