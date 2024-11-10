85debd4af72d2b9d1a7c24ebf574454142a47bcc
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-11-09 21:53:02 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Main•allfake.js
+++ plugins/Main•allfake.js
@@ -96,7 +96,7 @@ global.redeshost = [canal4, dash, panel, status, discord].getRandom()
 
 //Imagen
 let category = "imagen"
-const db = './media/database/db.json'
+const db = './src/database/db.json'
 const db_ = JSON.parse(fs.readFileSync(db))
 const random = Math.floor(Math.random() * db_.links[category].length)
 const randomlink = db_.links[category][random]
