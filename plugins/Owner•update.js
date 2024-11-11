6a5ffd08494043d198dee15c3b5653f53d6cde8c
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-11-10 21:11:00 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Owner•update.js
+++ plugins/Owner•update.js
@@ -1,6 +1,6 @@
 import { execSync } from 'child_process'
 let handler = async (m, { conn, text }) => {
-try {
+
 await m.react(rwait)
 if (conn.user.jid == conn.user.jid) {
 let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
@@ -9,7 +9,8 @@ await m.react(done)
 } catch (e) {
 await m.react(error)
 await m.reply('🚩 Se han hecho cambios locales qué entran en conflicto con las Actualizaciones del Repositorio, Para actualizar, reinstala el Bot o realiza las actualizaciones manualmente.')
-}}}
+}}
+
 handler.help = ['update', 'actualizar']
 handler.tags = ['owner']
 handler.command = ['update', 'actualizar']
