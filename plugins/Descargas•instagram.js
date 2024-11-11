de1665e88719cf3d99ed892245c226b0f5308db6
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-11-11 12:19:36 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Descargas•instagram.js
+++ plugins/Descargas•instagram.js
@@ -2,7 +2,7 @@ import { igdl } from "ruhend-scraper"
 
 let handler = async (m, { args, conn }) => { 
 if (!args[0]) {
-return conn.reply(m.chat, '🍟 *Ingresa un link de Instagram*', m, rcanal)}
+return conn.reply(m.chat, '🚩 Ingresa un link de Instagram.', m, rcanal)}
 try {
 await m.react(rwait)
 conn.reply(m.chat, `🕒 *Enviando El Video...*`, m, {
@@ -15,7 +15,7 @@ let res = await igdl(args[0])
 let data = res.data       
 for (let media of data) {
 await new Promise(resolve => setTimeout(resolve, 2000))           
-await conn.sendFile(m.chat, media.url, 'instagram.mp4', '🍟 *Tu video de instagram.*\n' + textbot, fkontak)
+await conn.sendFile(m.chat, media.url, 'instagram.mp4', '🚩 *Video de instagram.*\n' + textbot, m)
 }} catch {
 await m.react(error)
 conn.reply(m.chat, '🚩 Ocurrió un error.', m, fake)}}
