8998cdec643cca66b9fd1637a76e0b75d2798f6b
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-11-10 21:52:18 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Rg•unreg.js
+++ plugins/Rg•unreg.js
@@ -1,6 +1,9 @@
 let handler = async (m, { conn, text }) => {
 
 let user = global.db.data.users[m.sender]
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let mentionedJid = [who]
+let perfil = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://qu.ax/QGAVS.jpg')
 
 user.registered = false
 
@@ -9,7 +12,7 @@ await conn.sendMessage(global.channelid, { text: txt, contextInfo: {
 externalAdReply: {
 title: "【 🔔 𝗡𝗢𝗧𝗜𝗙𝗜𝗖𝗔𝗖𝗜𝗢́𝗡 🔔 】",
 body: '😿 ¡Un usuario menos en mi base de datos!',
-thumbnailUrl: fotoperfil,
+thumbnailUrl: perfil,
 sourceUrl: redes,
 mediaType: 1,
 showAdAttribution: false,
