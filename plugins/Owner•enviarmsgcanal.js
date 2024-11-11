e37cfcd132bebac4b8fbcdcbc66fea73bb70faf9
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-11-11 13:02:37 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Owner•enviarmsgcanal.js
+++ plugins/Owner•enviarmsgcanal.js
@@ -1,12 +1,18 @@
 const handler = async (m, {conn, text}) => {
 
-if (!text) return await conn.reply(m.chat, `🚩 Y el texto que quiere transmitir al canal?`, m)
+let q = m.quoted ? m.quoted : m
+let img = await q.download()
+if (!img) return await conn.reply(m.chat, '🚩 Te faltó la foto', m)
+
+else {
+if (!text) return await conn.reply(m.chat, `🚩 Y el texto que quiere transmitir al canal?`, m)}
 
 try {
 await m.react(rwait)
 
 await conn.reply(m.chat, `🚩 El texto se envió correctamente al canal.`, m, fake)
-await conn.reply(global.channelid, text, null, fake)
+//await conn.reply(global.channelid, text, null, fake)
+await conn.sendFile(global.channelid, img, 'error.jpg', text, m, null, rcanal);
 await m.react(done)
 
 } catch {
