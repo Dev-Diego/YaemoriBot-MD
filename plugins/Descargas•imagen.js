0b5ae1e7d4b2457fac674e5c8f89bdd470285afe
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-11-10 21:07:14 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Descargas•imagen.js
+++ plugins/Descargas•imagen.js
@@ -43,7 +43,7 @@ deviceListMetadata: {},
 deviceListMetadataVersion: 2
 },
 interactiveMessage: proto.Message.InteractiveMessage.fromObject({contextInfo: {mentionedJid: [m.sender]},
-body: proto.Message.InteractiveMessage.Body.create({ text: dev }),
+body: proto.Message.InteractiveMessage.Body.create({ text: `${dev}` }),
 footer: proto.Message.InteractiveMessage.Footer.create({ text: 'Imagen Busqueda' }),
 header: proto.Message.InteractiveMessage.Header.create({ hasMediaAttachment: false }),
 carouselMessage: proto.Message.InteractiveMessage.CarouselMessage.fromObject({ cards: [...Free] })
