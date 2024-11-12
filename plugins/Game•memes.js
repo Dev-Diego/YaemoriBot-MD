cf22fbadd989411892531837f487a678514f48d2
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-11-11 19:44:55 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Game•memes.js
+++ plugins/Game•memes.js
@@ -2,7 +2,7 @@
 import hispamemes from 'hispamemes'
 let handler = async (m, { conn, usedPrefix, command }) => {
 const meme = hispamemes.meme()
-conn.sendFile(m.chat, meme, '', '', fkontak)
+conn.sendFile(m.chat, meme, '', '', m, null, rcanal)
 m.react(emoji2)
 }
 handler.help = ['meme']
