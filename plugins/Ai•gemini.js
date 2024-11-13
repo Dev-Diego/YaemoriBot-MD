3386c8a6492b444c4a065c9844dc5e8833bd1b4f
Patito Ofc <theyaemoribot@gmail.com>
2024-11-12 22:12:37 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Ai•gemini.js
+++ plugins/Ai•gemini.js
@@ -5,10 +5,10 @@ var handler = async (m, { text,  usedPrefix, command }) => {
 if (!text) { return conn.reply(m.chat, `❀ Ingrese una petición para que Gemini lo responda.`, m)}
 try {
 await m.react(rwait)
-const { key } = await conn.sendMessage(m.chat, {text: `❀ Gemini está procesando tu petición, espera unos segundos.`}, {quoted: m})
 var apii = await fetch(`https://apis-starlights-team.koyeb.app/starlight/gemini?text=${text}`)
 var res = await apii.json()
-await conn.sendMessage(m.chat, {text: res.result, edit: key})
+await conn.reply(m.chat, res.result, m)
+await m.react(done)
 } catch {
 await m.react(error)
 await conn.reply(m.chat, `✘ Gemini no puede responder a esa pregunta.`, m, rcanal)
