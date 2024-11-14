50f0a65b317c4939bc6d79a21c1ea23b33e54179
Patito Ofc <theyaemoribot@gmail.com>
2024-11-14 09:06:20 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Game•simi.js
+++ plugins/Game•simi.js
@@ -7,9 +7,8 @@ const handler = async (m, {conn, text, command, args, usedPrefix}) => {
 if (!text) { return conn.reply(m.chat, `❀ Ingrese una petición para que Simi lo responda.`, m)};
 try {
 await m.react(rwait)
-const { key } = await conn.sendMessage(m.chat, {text: `❀ Simi está procesando tu petición, espera unos segundos.`}, {quoted: m});
 const resSimi = await simitalk(text);
-await conn.sendMessage(m.chat, {text: resSimi.resultado.simsimi, edit: key});
+await conn.sendMessage(m.chat, {text: resSimi.resultado.simsimi, {quoted: m});
 await m.react(done)
 } catch {
 await m.react(error)
