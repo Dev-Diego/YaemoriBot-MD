819a3b393d682b4e69e05f5816df2e710f4cd11a
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-11-16 15:50:30 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Owner•join.js
+++ plugins/Owner•join.js
@@ -1,19 +1,15 @@
 let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})( [0-9]{1,3})?/i
 
-let handler = async (m, { conn, text, isOwner, usedPrefix, command, participants, groupMetadata }) => {
+let handler = async (m, { conn, text, isOwner, usedPrefix, command }) => {
 
-let user = m.sender.split('@')[0] 
-let link = (m.quoted ? m.quoted.text ? m.quoted.text : text : text) || text
-let [_1, code1] = link.match(linkRegex) || []
-if (!code1) return m.reply(`🚩 Ingresa el enlace del Grupo.`)
-try {      
-if ( isOwner || m.fromMe) {
-await m.reply(`🚩 Me uní correctamente al grupo.`)
-let res1 = await conn.groupAcceptInvite(code1)
-await conn.sendMessage(res1, { text: `${packname}\n🥳 Se ha unido al grupo.\n\n🐢 *Fué agregada por:* @${user}`, mentions: (await conn.groupMetadata(`${res1}`)).participants.map(v => v.id) }, [user], { quoted: fkontak })
-}} catch (e) {
-await m.reply('🚩 Ocurrió un error')
-}}
+if (!text) return m.reply(`🚩 Ingresa el enlace del Grupo.`)
+try {
+let [_, code] = text.match(linkRegex) || []
+if (!code) return m.reply('🐢 Enlace invalido.')
+let res = await conn.groupAcceptInvite(code)
+m.reply(`🍭 Me uní correctamente al Grupo`)
+} catch {
+return m.reply('✘ Ocurrió un error.')}}
 
 handler.help = ['join <link>']
 handler.tags = ['owner']
