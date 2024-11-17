eeeb15c5225dfae64dcb76326f77cdae686a9b89
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-11-17 12:31:05 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Owner•backup.js
+++ plugins/Owner•backup.js
@@ -8,7 +8,7 @@ let d = new Date
 let date = d.toLocaleDateString('es', { day: 'numeric', month: 'long', year: 'numeric' })
 let database = await fs.readFileSync(`./src/database/database.json`)
 let creds = await fs.readFileSync(`./${sessions}/creds.json`)
-await conn.reply(m.chat, `*• Fecha:* ${date}`, m, fake)
+await conn.reply(m.chat, `*• Fecha:* ${date}`, m)
 await conn.sendMessage(m.sender, {document: database, mimetype: 'application/json', fileName: `database.json`}, { quoted: fkontak })
 await m.react(done)
 await conn.sendMessage(m.sender, {document: creds, mimetype: 'application/json', fileName: `creds.json`}, { quoted: fkontak })
