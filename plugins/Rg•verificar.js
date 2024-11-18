22620c87f5770a3d28a09f3e5754a862e34afa4c
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-11-18 08:50:05 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Rg•verificar.js
+++ plugins/Rg•verificar.js
@@ -44,7 +44,7 @@ await m.react('📩')
 await conn.sendMini(m.chat, '⊱『✅𝆺𝅥 𝗥𝗘𝗚𝗜𝗦𝗧𝗥𝗔𝗗𝗢(𝗔) 𝆹𝅥✅』⊰', textbot, regbot, imagen1, imagen1, channel, m)
 let chtxt = `
 👤 *Usuario* » ${m.pushName || 'Anónimo'}
-🌎 *Pais* » ${global.userNationality}
+🌎 *Pais* » ${global.pais}
 🗃 *Verificación* » ${user.name}
 🌺 *Edad* » ${user.age}
 📆 *Fecha* » ${moment.tz('America/Bogota').format('DD/MM/YY')}
