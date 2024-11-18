98e98a45ce5fe719bfdac87e459f46a748f560c5
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-11-18 08:53:20 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Main•allfake.js
+++ plugins/Main•allfake.js
@@ -30,9 +30,9 @@ console.log(`Error : ${e}`)
 
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 global.fotoperfil = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://qu.ax/QGAVS.jpg')
-let api = await axios.get(`https://deliriussapi-oficial.vercel.app/tools/country?text=${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}`)
-let PaisData = api.data.result
-global.pais = PaisData ? `${userNationalityData.name} ${userNationalityData.emoji}` : 'Desconocido'
+let delirius = await axios.get(`https://deliriussapi-oficial.vercel.app/tools/country?text=${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}`)
+let PaisData = delirius.data.result
+global.pais = PaisData ? `${PaisData.name} ${PaisData.emoji}` : 'Desconocido'
 let user = global.db.data.users[who]
 let pushname = m.pushName || 'Anónimo'
 
