69db3680fd972e17ab73ed08785a63f4fd08f102
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-11-19 20:54:34 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Info•autoresponder.js
+++ plugins/Info•autoresponder.js
@@ -4,12 +4,12 @@ conn.reply(m.chat, "❌ *No tienes permitido personalizar la autorespuesta del b
 }
 const chatData = global.db.data.chats[m.chat]
 if (text) {
-if (chatData.sAutorespond) return conn.reply(m.chat, `⚠️ *Actualmente hay el siguiente prompt en uso:*\n\n${chatData.sAutorespond}\n\n💡 *Si quieres cambiarlo, usa el comando sin texto para borrar el prompt actual y luego establece el nuevo prompt.*`, m)
+if (chatData.sAutoresponder) return conn.reply(m.chat, `⚠️ *Actualmente hay el siguiente prompt en uso:*\n\n${chatData.sAutorespond}\n\n💡 *Si quieres cambiarlo, usa el comando sin texto para borrar el prompt actual y luego establece el nuevo prompt.*`, m)
 
-chatData.sAutorespond = text
+chatData.sAutoresponder = text
 conn.reply(m.chat, `✅ *Configuración exitosa.*\n\n*Has establecido un nuevo prompt para este chat.*\n💬 A partir de ahora, activa usando *${usedPrefix}on autoresponder*, el bot usará las indicaciones que hayas establecido.\n\n> *Recuerda etiquetar o responder a un mensaje del bot para que te responda.*`, m)
 } else {
-if (chatData.sAutorespond) {
+if (chatData.sAutoresponder) {
 chatData.sAutorespond = ''
 conn.reply(m.chat, "🗑️ *Prompt borrado con éxito.*", m)
 } else {
