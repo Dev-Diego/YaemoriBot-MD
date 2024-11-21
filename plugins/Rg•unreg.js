86b2af59ef5a84bb389b96228d5d7929ecff39f8
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-11-21 12:26:29 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Rg•unreg.js
+++ plugins/Rg•unreg.js
@@ -1,17 +1,17 @@
 let handler = async (m, { conn, usedPrefix, args, text }) => {
 
-let user = global.db.data.users[m.sender]
-
 if (!args[0]) return conn.reply(m.chat `🚩 Te hace falta el numero de registro. Para ver el numero de registro utiliza:\n${usedPrefix}nserie`, m, rcanal)
+
+let user = global.db.data.users[m.sender]
 
 let sn = createHash('md5').update(m.sender).digest('hex')
 if (args[0] !== sn) return m.reply('⚠️ *Número de registro incorrecto.*')
 
+user.registered = false
 global.db.data.users[m.sender].money -= 600
 global.db.data.users[m.sender].cookies -= 15
 global.db.data.users[m.sender].exp -= 245
 global.db.data.users[m.sender].joincount -= 5
-user.registered = false
 
 return conn.reply(m.chat, `🚩 Tu registro ha sido anulado correctamente.`, m, rcanal)
 
