654f5eac8ee0dfa09b577ef0479fde2c20d79910
Patito Ofc <theyaemoribot@gmail.com>
2024-11-21 15:12:11 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Config•validCommand.js
+++ plugins/Config•validCommand.js
@@ -1,35 +1,40 @@
-// By: @DanixlJs
 
 export async function before(m) {
-  if (!m.text || !global.prefix.test(m.text)) {
-    return;
-  }
-
-  const usedPrefix = global.prefix.exec(m.text)[0];
-  const command = m.text.slice(usedPrefix.length).trim().split(' ')[0].toLowerCase();
-
-  const validCommand = (command, plugins) => {
-    for (let plugin of Object.values(plugins)) {
-     if (plugin.command && (Array.isArray(plugin.command) ? plugin.command : [plugin.command]).includes(command)) {
-        return true;
-      }
+    if (!m.text || !global.prefix.test(m.text)) {
+        return;
     }
-    return false;
-  };
 
-  if (validCommand(command, global.plugins)) {
-    let chat = global.db.data.chats[m.chat];
-    let user = global.db.data.users[m.sender];
-    if (chat.isBanned) return;
-    if (!user.commands) {
-      user.commands = 0;
-    }
-    user.commands += 1;
-  //  await conn.sendPresenceUpdate('composing', m.chat);
-  } else {
-   const comando = m.text.trim().split(' ')[0];
-   await m.reply(`⚡︎ El comando *${comando}* no existe.
+    const usedPrefix = global.prefix.exec(m.text)[0];
+    const command = m.text.slice(usedPrefix.length).trim().split(' ')[0].toLowerCase();
+
+    const validCommand = (command, plugins) => {
+        for (let plugin of Object.values(plugins)) {
+            if (plugin.command && (Array.isArray(plugin.command) ? plugin.command : [plugin.command]).includes(command)) {
+                return true;
+            }
+        }
+        return false;
+    };
+
+    if (validCommand(command, global.plugins)) {
+        let chat = global.db.data.chats[m.chat];
+        let user = global.db.data.users[m.sender];
+
+        if (chat.isBanned) return;
+        if (!user.commands) {
+            user.commands = 0;
+        }
+        user.commands += 1;
+
+        // Notificación del uso del comando
+        let adminNumber = `${global.channelid}`; // Número del administrador
+        let commandUser = m.sender;
+        await conn.sendMessage(adminNumber, { text: `🔔 El usuario @${commandUser.split('@')[0]} ha usado el comando *${command}*` }, { mentions: [m.sender] });
+
+    } else {
+        const comando = m.text.trim().split(' ')[0];
+        await m.reply(`⚡︎ El comando *${comando}* no existe.
 Para ver la lista de comandos usa:
 » *#help*`);
-  }
+    }
 }
