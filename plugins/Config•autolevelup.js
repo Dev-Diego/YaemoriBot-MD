1f589a7522049cf0e76184a6821db82610e53df5
Patito Ofc <theyaemoribot@gmail.com>
2024-11-22 18:26:12 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Config•autolevelup.js
+++ plugins/Config•autolevelup.js
@@ -5,12 +5,24 @@ export function before(m, { conn }) {
 
 let user = global.db.data.users[m.sender]
 let chat = global.db.data.chats[m.chat]
+let userName = m.pushName || 'Anónimo'
 if (!chat.autolevelup)
 return !0
 
 let before = user.level * 1
 while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
 if (before !== user.level) {
+await conn.sendMessage(global.channelid, { text: `👤 *Usuario:* ${userName}\n🐢 *Nivel anterior:* ${before}\n✨️ *Nivel actual:* ${user.level}\n👾 *Rango:* ${user.role}`, contextInfo: {
+externalAdReply: {
+title: "【 🔔 𝗡𝗢𝗧𝗜𝗙𝗜𝗖𝗔𝗖𝗜𝗢́𝗡 🔔 】",
+body: '🥳 ¡Un usuario tiene un nuevo nivel!',
+thumbnailUrl: perfil,
+sourceUrl: redes,
+mediaType: 1,
+showAdAttribution: false,
+renderLargerThumbnail: false
+}}}, { quoted: null })
+
 m.reply(`*🎉 ¡ F E L I C I D A D E S ! 🎉*\n\n💫 Nivel Actual » *${user.level}*\n🌵 Rango » *${user.role}*\n📆 Fecha » *${moment.tz('America/Bogota').format('DD/MM/YY')}*\n\n> *\`¡Has alcanzado un Nuevo Nivel!\`*
 `.trim())
     }
