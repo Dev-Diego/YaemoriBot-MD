444c544af682c0c2107b032b57869507f8014227
Patito Ofc <theyaemoribot@gmail.com>
2024-11-22 08:49:15 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Config•welcome.js
+++ plugins/Config•welcome.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 
 export async function before(m, {conn, participants, groupMetadata}) {
   if (!m.messageStubType || !m.isGroup) return !0;
-  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://ibb.co/WBYqRDd')
+  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://qu.ax/QGAVS.jpg')
   let img = await (await fetch(`${pp}`)).buffer()
   let chat = global.db.data.chats[m.chat]
   
@@ -13,17 +13,17 @@ export async function before(m, {conn, participants, groupMetadata}) {
   let userName = user ? user.name : await conn.getName(who);
 
   if (chat.welcome && m.messageStubType == 27) {
-    let bienvenida = `ゲ◜៹ New Member ៹◞ゲ \n\nUsuario\n» @${m.messageStubParameters[0].split`@`[0]} \nGrupo\n» ${groupMetadata.subject}\n${dev}`;
+    let bienvenida = `ゲ◜៹ New Member ៹◞ゲ \n\nUsuario: @${m.messageStubParameters[0].split`@`[0]} \nGrupo: ${groupMetadata.subject}\n${dev}`;
     
-await conn.sendMini(m.chat, packname, textbot, bienvenida, img, img, channel, estilo)
+await conn.sendMini(m.chat, packname, dev, bienvenida, img, img, channel, estilo)
   }
   
   if (chat.welcome && m.messageStubType == 28) {
-    let bye = `ゲ◜៹ Bye Member ៹◞ゲ \n\nUsuario\n» @${m.messageStubParameters[0].split`@`[0]}\nGrupo\n» ${groupMetadata.subject}\n${dev}`;
-await conn.sendMini(m.chat, packname, textbot, bye, img, img, channel, estilo)
+    let bye = `ゲ◜៹ Bye Member ៹◞ゲ \n\nUsuario: @${m.messageStubParameters[0].split`@`[0]}\nGrupo: ${groupMetadata.subject}\n${dev}`;
+await conn.sendMini(m.chat, packname, dev, bye, img, img, channel, estilo)
   }
   
   if (chat.welcome && m.messageStubType == 32) {
-    let kick = `ゲ◜៹ Bye Member ៹◞ゲ \n\nUsuario\n» @${m.messageStubParameters[0].split`@`[0]}\nGrupo\n» ${groupMetadata.subject}\n${dev}`;
-await conn.sendMini(m.chat, packname, textbot, kick, img, img, channel, estilo)
+    let kick = `ゲ◜៹ Bye Member ៹◞ゲ \n\nUsuario: @${m.messageStubParameters[0].split`@`[0]}\nGrupo: ${groupMetadata.subject}\n${dev}`;
+await conn.sendMini(m.chat, packname, dev, kick, img, img, channel, estilo)
 }}
