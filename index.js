0d97432cb8ec14779e2d79314e48f1924c11059d
Patito Ofc <theyaemoribot@gmail.com>
2024-11-23 14:13:52 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- index.js
+++ index.js
@@ -1,84 +1,80 @@
-import { join, dirname } from 'path'
-import { createRequire } from 'module'
-import { fileURLToPath } from 'url'
-import boxen from 'boxen'
+import { join, dirname } from 'path';
+import { createRequire } from 'module';
+import { fileURLToPath } from 'url';
 import { setupMaster, fork } from 'cluster';
 import { watchFile, unwatchFile } from 'fs';
-import cfonts from 'cfonts'
-import { createInterface } from 'readline'
-import yargs from 'yargs'
-import chalk from 'chalk'
-console.log('\n✰ Iniciando YaemoriBot ✰')
-const __dirname = dirname(fileURLToPath(import.meta.url))
-const require = createRequire(__dirname)
-const { name, description, collaborators, author, version } = require(join(__dirname, './package.json'))
-const { say } = cfonts
-const rl = createInterface(process.stdin, process.stdout)
-const subtitleStyle = chalk.white.bold
-const responseStyle = chalk.dim.bold
-
-let activeCollaborators = ''
-for (const key in collaborators) {
-if (collaborators.hasOwnProperty(key)) {
-activeCollaborators += collaborators[key] + ', '
-}}
-activeCollaborators = activeCollaborators.slice(0, -2);
-cfonts.say('yaemori\nbot md', {
-align: 'center',           
-gradient: ['red', 'blue'] 
-})
-cfonts.say(description, {
+import cfonts from 'cfonts';
+import { createInterface } from 'readline';
+import yargs from 'yargs';
+import chalk from 'chalk';
+console.log('\n✰ Iniciando YaemoriBot ✰');
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const require = createRequire(__dirname);
+const { name, description, author, version } = require(join(__dirname, './package.json'));
+const { say } = cfonts;
+const rl = createInterface(process.stdin, process.stdout);
+say('Yaemori\nBot-MD', {
+font: 'block',
+align: 'center',
+colors: ['red']
+});
+say(`Multi Device`, {
+font: 'chrome',
+align: 'center',
+colors: ['red']
+});
+say(`Developed By • DevDiego`, {
 font: 'console',
 align: 'center',
-gradient: ['blue', 'magenta']
-})
-const message = `${subtitleStyle('Desarrollado por »')} ${responseStyle(author.name)}
-${subtitleStyle('Código basado por »')} ${responseStyle('BrunoSobrino')}
-${subtitleStyle('Colaboradores activos »')} ${responseStyle(activeCollaborators)}
-${subtitleStyle('Versión »')} ${responseStyle(version)}`
-console.log(boxen(message, { padding: 1, margin: 1, borderStyle: 'double', borderColor: 'blue', float: 'center', }))
-var isRunning = false
+colors: ['yellow']
+});
+var isRunning = false;
 function start(file) {
-if (isRunning) return
-isRunning = true
-let args = [join(__dirname, file), ...process.argv.slice(2)]
+if (isRunning) return;
+isRunning = true;
+let args = [join(__dirname, file), ...process.argv.slice(2)];
+say([process.argv[0], ...args].join(' '), {
+font: 'console',
+align: 'center',
+colors: ['green']
+});
 setupMaster({
 exec: args[0],
 args: args.slice(1),
-})
-let p = fork()
+});
+let p = fork();
 p.on('message', data => {
 switch (data) {
 case 'reset':
-p.process.kill()
-isRunning = false
-start.apply(this, arguments)
-break
+p.process.kill();
+isRunning = false;
+start.apply(this, arguments);
+break;
 case 'uptime':
-p.send(process.uptime())
-break
+p.send(process.uptime());
+break;
 }
-})
+});
 p.on('exit', (_, code) => {
-isRunning = false
-console.error('🚩 Error:\n', code)
-process.exit()
-if (code === 0) return
+isRunning = false;
+console.error('🚩 Error:\n', code);
+process.exit();
+if (code === 0) return;
 watchFile(args[0], () => {
-unwatchFile(args[0])
-start(file)
-})
-})
-let opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
+unwatchFile(args[0]);
+start(file);
+});
+});
+let opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse());
 if (!opts['test'])
 if (!rl.listenerCount()) rl.on('line', line => {
-p.emit('message', line.trim())
-})
+p.emit('message', line.trim());
+});
 }
 process.on('warning', (warning) => {
 if (warning.name === 'MaxListenersExceededWarning') {
-console.warn('🚩 Se excedió el límite de Listeners en:')
-console.warn(warning.stack)
+console.warn('🚩 Se excedió el límite de Listeners en:');
+console.warn(warning.stack);
 }
-})
-start('start.js')
+});
+start('start.js');
