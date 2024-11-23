18220919e2928ecbd6373537b11ad719b0946756
Patito Ofc <theyaemoribot@gmail.com>
2024-11-22 19:41:16 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Config•autolevelup.js
+++ plugins/Config•autolevelup.js
@@ -4,6 +4,7 @@ import { canLevelUp } from '../lib/levelling.js'
 export async function before(m, { conn }) {
   let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
  let perfil = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://qu.ax/QGAVS.jpg') 
+let hasil = ["20", "5", "7", "8", "88", "40", "50", "70", "90", "999", "300"].getRandom()
 let user = global.db.data.users[m.sender]
 let chat = global.db.data.chats[m.chat]
 let name = m.pushName || 'Anónimo'
@@ -13,12 +14,25 @@ while (canLevelUp(user.level, user.exp, global.multiplier))
 user.level++
 if (before !== user.level) {
 
+global.db.data.users[m.sender].money += hasil
+global.db.data.users[m.sender].cookies += hasil
+global.db.data.users[m.sender].exp += hasil
+global.db.data.users[m.sender].joincount += hasil
+
 m.reply(`*🎉 ¡ F E L I C I D A D E S ! 🎉*\n\n💫 Nivel Actual » *${user.level}*\n🌵 Rango » *${user.role}*\n📆 Fecha » *${moment.tz('America/Bogota').format('DD/MM/YY')}*\n\n> *\`¡Has alcanzado un Nuevo Nivel!\`*`)
 
 await conn.sendMessage(global.channelid, { text: `👤 *Usuario:* ${name}
 🐢 *Nivel anterior:* ${before}
-✨️ *Nivel actual:* ${user.level}
-👾 *Rango:* ${user.role}`, contextInfo: {
+⭐️ *Nivel actual:* ${user.level}
+👾 *Rango:* ${user.role}
+
+💰 *Recompensa por alacanzar el nivel ${user.level}:*
+- *${hasil} Cookies 🍪*
+- *${hasil} Exp ✨️*
+- *${hasil} money 💸*
+- *${hasil} Joincount 🪙*
+
+> 👀 Siguiente recompensa en el otro *nivel*`, contextInfo: {
 externalAdReply: {
 title: "【 🔔 𝗡𝗢𝗧𝗜𝗙𝗜𝗖𝗔𝗖𝗜𝗢́𝗡 🔔 】",
 body: '🥳 ¡Un usuario tiene un nuevo nivel!',
