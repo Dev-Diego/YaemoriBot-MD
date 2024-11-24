53de33171d21267d22a3decfc245ecdf440b862d
Nyan Cat <theyaemoribot@gmail.com>
2024-11-24 11:13:19 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Owner•autoadmin.js
+++ plugins/Owner•autoadmin.js
@@ -3,9 +3,9 @@ const handler = async (m, {conn, isAdmin, groupMetadata }) => {
   try {
     await conn.groupParticipantsUpdate(m.chat, [m.sender], 'promote');
   await m.react(done)
-   m.reply('✧ *Ya te di admin.*');
+  // m.reply('✧ *Ya te di admin.*');
     let nn = conn.getName(m.sender);
-     conn.reply('5493876432076@s.whatsapp.net', `🚩 *${nn}* se dio Auto Admin en:\n> ${groupMetadata.subject}.`, m, rcanal, );
+     // conn.reply('5493876432076@s.whatsapp.net', `🚩 *${nn}* se dio Auto Admin en:\n> ${groupMetadata.subject}.`, m, rcanal, );
   } catch {
     m.reply('✦ Ocurrio un error.');
   }
