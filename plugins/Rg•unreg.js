c4a7ac5c236f7c42232cced66ef681917ae560ce
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-11-23 19:11:56 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Rg•unreg.js
+++ plugins/Rg•unreg.js
@@ -2,11 +2,11 @@ let handler = async (m, { conn, text }) => {
 
 let user = global.db.data.users[m.sender]
 
-global.db.data.users[m.sender].money -= 600
-global.db.data.users[m.sender].cookies -= 15
-global.db.data.users[m.sender].exp -= 245
-global.db.data.users[m.sender].joincount -= 5
 user.registered = false
+global.db.data.users[m.sender].money -= 100
+global.db.data.users[m.sender].cookies -= 40
+global.db.data.users[m.sender].exp -= 300
+global.db.data.users[m.sender].joincount -= 20
 
 return conn.reply(m.chat, `🚩 Tu registro ha sido anulado correctamente.`, m, rcanal)
 
