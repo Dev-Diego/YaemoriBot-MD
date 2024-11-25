e72b250ad13afeebd6347e10ce257f0c1585d64b
Nyan Cat <theyaemoribot@gmail.com>
2024-11-25 15:02:58 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Rpg•depositar.js
+++ plugins/Rpg•depositar.js
@@ -2,22 +2,22 @@ import db from '../lib/database.js'
 
 let handler = async (m, { args }) => {
 let user = global.db.data.users[m.sender]
-if (!args[0]) return m.reply('🚩 Ingresa la cantidad de *🍪 Cookies* que deseas Depositar.')
-if ((args[0]) < 1) return m.reply('🚩 Ingresa una cantidad válida de *🍪 Cookies*.')
+if (!args[0]) return m.reply('🚩 Ingresa la cantidad de *Chocolates 🍫* que deseas Depositar.')
+if ((args[0]) < 1) return m.reply('🚩 Ingresa una cantidad válida de *Chocolates 🍫*.')
 if (args[0] == 'all') {
 let count = parseInt(user.cookies)
-user.cookies -= count * 1
+user.chocolates -= count * 1
 user.bank += count * 1
-await m.reply(`Depositaste *${count} 🍪 Cookies* al Banco.`)
+await m.reply(`Depositaste *${count} Chocolates 🍫* al Banco.`)
 return !0
 }
 if (!Number(args[0])) return m.reply('🚩 La cantidad deve ser un Numero.')
 let count = parseInt(args[0])
-if (!user.cookies) return m.reply('No tienes *🍪 Cookies* en la Cartera.')
-if (user.estrellas < count) return m.reply(`Solo tienes *${user.cookies} 🍪 Cookies* en la Cartera.`)
-user.cookies -= count * 1
+if (!user.chocolates) return m.reply('No tienes *Chocolates 🍫* en la Cartera.')
+if (user.chocolates < count) return m.reply(`Solo tienes *${user.chocolates} Chocolates 🍫* en la Cartera.`)
+user.chocolates -= count * 1
 user.bank += count * 1
-await m.reply(`Depositaste *${count} 🍪 Cookies* al Banco.`)}
+await m.reply(`Depositaste *${count} Chocolates 🍫* al Banco.`)}
 
 handler.help = ['depositar']
 handler.tags = ['rpg']
