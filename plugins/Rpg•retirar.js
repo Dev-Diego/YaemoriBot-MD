29122057c0f7e621ce0d7a00e40cde4f2940f361
Nyan Cat <theyaemoribot@gmail.com>
2024-11-25 14:58:53 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Rpg•retirar.js
+++ plugins/Rpg•retirar.js
@@ -2,21 +2,21 @@ import db from '../lib/database.js'
 
 let handler = async (m, { args }) => {
 let user = global.db.data.users[m.sender]
-if (!args[0]) return m.reply('🚩 Ingresa la cantidad de *🍪 Cookies* que deseas Retirar.')
+if (!args[0]) return m.reply('🚩 Ingresa la cantidad de *Chocolates 🍫* que deseas Retirar.')
 if (args[0] == 'all') {
 let count = parseInt(user.bank)
 user.bank -= count * 1
-user.cookies += count * 1
-await m.reply(`🚩 Retiraste *${count} 🍪 Cookies* del Banco.`)
+user.chocolates += count * 1
+await m.reply(`🚩 Retiraste *${count} Chocolates 🍫* del Banco.`)
 return !0
 }
 if (!Number(args[0])) return m.reply('🚩 La cantidad deve ser un Numero.')
 let count = parseInt(args[0])
-if (!user.bank) return m.reply('No tienes *🍪 Cookies* en el Banco.')
-if (user.bank < count) return m.reply(`Solo tienes *${user.bank} 🍪 Cookies* en el Banco.`)
+if (!user.bank) return m.reply('No tienes *Chocolates 🍫* en el Banco.')
+if (user.bank < count) return m.reply(`Solo tienes *${user.bank} Chocolates 🍫* en el Banco.`)
 user.bank -= count * 1
-user.cookies += count * 1
-await m.reply(`🚩 Retiraste *${count} 🍪 Cookies* del Banco.`)}
+user.chocolates += count * 1
+await m.reply(`🚩 Retiraste *${count} Chocolates 🍫* del Banco.`)}
 
 handler.help = ['retirar']
 handler.tags = ['rpg']
