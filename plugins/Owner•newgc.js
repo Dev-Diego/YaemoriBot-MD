a937d4ad4b9fea62fc86f156c212e4ea4a0bf4f4
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-11-25 09:28:31 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Owner•newgc.js
+++ plugins/Owner•newgc.js
@@ -1,10 +1,11 @@
+
 let handler = async (m, { conn, text }) => {
 if (!text) return m.reply('🚩 Ingresa un nombre para el grupo.')
 try{
 m.reply('🚩 *Creando grupo*')
 let group = await conn.groupCreate(text, [m.sender])
 let link = await conn.groupInviteCode(group.gid)
-m.reply('https://chat.whatsapp.com/' + link)
+m.reply('https://chat.whatsapp.com/' + url)
 } catch (e) {
 m.reply(`🚩 Ocurrió un error.`)
 }
