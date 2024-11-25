6c900a13a53b15ece3e4262854126ec78df4c208
Nyan Cat <theyaemoribot@gmail.com>
2024-11-25 14:41:19 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Game•cf.js
+++ plugins/Game•cf.js
@@ -18,11 +18,11 @@ let handler = async (m, { conn, text, command, usedPrefix }) => {
     let esGanador = text.toLowerCase() === resultado
 
     if (esGanador) {
-        global.db.data.users[m.sender].cookies += 1000
-        conn.reply(m.chat, `✿︎ La moneda cayó en *${text}*, acabas de ganar *1000 🍪 Galletas*`, m, rcanal)       
+        global.db.data.users[m.sender].chocolates += 1000
+        conn.reply(m.chat, `✿︎ La moneda cayó en *${text}*, acabas de ganar *1000 Chocolates 🍫*`, m, rcanal)       
     } else {
-        global.db.data.users[m.sender].cookies -= 500
-        conn.reply(m.chat, `✿︎ La moneda cayó en *${text}*, acabas de perder *500 🍪 Galletas*`, m, rcanal)
+        global.db.data.users[m.sender].chocolates -= 500
+        conn.reply(m.chat, `✿︎ La moneda cayó en *${text}*, acabas de perder *500 Chocolates 🍫*`, m, rcanal)
     }
 }
 
