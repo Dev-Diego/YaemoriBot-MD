02c97e26f393a23ec0a8a8250382c23bdf8871d4
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-11-25 09:27:16 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Owner•newgc.js
+++ plugins/Owner•newgc.js
@@ -1,21 +1,17 @@
 let handler = async (m, { conn, text }) => {
-
-if (!text) return m.reply('🚩 Ingresa un nombre para el grupo.');
-    
-try {
-m.reply('🚩 *Creando grupo*');
-let group = await conn.groupCreate(text, [m.sender]);
-let link = await conn.groupInviteCode(group.gid);
-m.reply(`🚩 Grupo creado con éxito. Aquí está el enlace de invitación: https://chat.whatsapp.com/${link}`);
+if (!text) return m.reply('🚩 Ingresa un nombre para el grupo.')
+try{
+m.reply('🚩 *Creando grupo*')
+let group = await conn.groupCreate(text, [m.sender])
+let link = await conn.groupInviteCode(group.gid)
+m.reply('https://chat.whatsapp.com/' + link)
 } catch (e) {
-m.reply(`🚩 Ocurrió un error al crear el grupo.`);
-};
+m.reply(`🚩 Ocurrió un error.`)
 }
-
-handler.help = ['grupocrear <nombre>'];
-handler.tags = ['owner'];
-handler.command = ['creargc', 'newgc', 'creargrupo', 'grupocrear'];
-handler.rowner = true;
-handler.register = true;
-
-export default handler;
+}
+handler.help = ['grupocrear <nombre>']
+handler.tags = ['owner']
+handler.command = ['creargc', 'newgc', 'creargrupo', 'grupocrear']
+handler.rowner = true
+handler.register = true
+export default handler
