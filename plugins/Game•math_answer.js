5383a52e808cefdda218391bf0372a285fb998f3
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-11-25 16:56:51 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Game•math_answer.js
+++ plugins/Game•math_answer.js
@@ -4,20 +4,20 @@ const handler = async (m, {conn}) => {
   if (!m.quoted) return;
   if (m.quoted.sender != conn.user.jid) return;
   if (!/^Cuanto es el resultado de/i.test(m.quoted.text)) return;
-  if (!(m.chat in global.math)) return conn.reply(m.chat, `🌵 Ya se ha respondido a esa pregunta.`, m, rcanal);
+  if (!(m.chat in global.math)) return conn.reply(m.chat, `🍄 Ya se ha respondido a esa pregunta.`, m, rcanal);
   if (m.quoted.id == global.math[id][0].id) {
     const math = global.math[id][1];
     if (m.text == math.result) {
-      conn.reply(m.chat, `🌵 Respuesta correcta.\n🎁 Premio: *${math.bonus} Galletas*`, m, rcanal);
-      global.db.data.users[m.sender].cookies += math.bonus;
+      conn.reply(m.chat, `😺 Respuesta correcta.\n🎁 Premio: *${math.bonus} Chocolates*`, m, rcanal);
+      global.db.data.users[m.sender].chocolates += math.bonus;
       clearTimeout(global.math[id][3]);
       delete global.math[id];
     } else {
       if (--global.math[id][2] == 0) {
-        conn.reply(m.chat, `🌵 Se acabaron tus oportunidades.\n⭐️ La respuesta es: *${math.result}*`, m, rcanal);
+        conn.reply(m.chat, `😿 Se acabaron tus oportunidades.\n🍄 La respuesta es: *${math.result}*`, m, rcanal);
         clearTimeout(global.math[id][3]);
         delete global.math[id];
-      } else conn.reply(m.chat, `🌵 Respuesta incorrecta.\n✨️ Oportunidades disponible: *${global.math[id][2]}*`, m, rcanal);
+      } else conn.reply(m.chat, `😾 Respuesta incorrecta.\n🍄 Oportunidades disponible: *${global.math[id][2]}*`, m, rcanal);
     }
   }
 };
