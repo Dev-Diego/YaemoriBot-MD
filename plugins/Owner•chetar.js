73cb42a1798deeaf2b1fac7994121b9e8b860efe
Nyan Cat <theyaemoribot@gmail.com>
2024-11-25 14:59:08 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Owner•chetar.js
+++ plugins/Owner•chetar.js
@@ -2,7 +2,7 @@ const handler = async (m, { conn }) => {
     const user = global.db.data.users[m.sender];
         conn.sendMessage(m.chat, {text: `🚩 *@${m.sender.split('@')[0]} Ahora tienes recursos ilimitados*`, mentions: [m.sender]}, {quoted: fkontak});
       global.db.data.users[m.sender].money = Infinity;
-    global.db.data.users[m.sender].cookies = Infinity;
+    global.db.data.users[m.sender].chocolates = Infinity;
   global.db.data.users[m.sender].level = Infinity;
  global.db.data.users[m.sender].exp = Infinity;
 };
